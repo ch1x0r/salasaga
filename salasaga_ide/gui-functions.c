d669f02d0281428ae98b21e19ef6a22c19a45181
Justin Clift <justin@salasaga.org>
2006-06-10 15:38:41 +0000

    Committed the code for setting and unsetting slide names, which I'd somehow managed to miss committing before.

--- salasaga_ide/gui-functions.c
+++ salasaga_ide/gui-functions.c
@@ -4282,11 +4282,103 @@ void slide_move_up(void)
 }
 
 
+// Function called when the user selects Slide -> Reset slide name from the top menu
+void slide_name_reset(void)
+{
+	// Frees the string used for storing the present slide name, and sets it to NULL to mark as 'unset'
+	g_string_free(((slide *) current_slide->data)->name, TRUE);
+	((slide *) current_slide->data)->name = NULL;
+
+	// Recreate the film strip tooltips
+	create_tooltips();
+}
+
+
+// Function called when the user selects Slide -> Edit slide name from the top menu
+void slide_name_set(void)
+{
+	// Local variables
+	GtkDialog			*slide_dialog;			// Widget for the dialog
+	GtkWidget			*dialog_table;			// Table used for neat layout of the dialog box
+	gint				dialog_result;			// Catches the return code from the dialog box
+
+	GtkWidget			*name_label;			// Label widget
+	GtkWidget			*name_entry;			// Widget for accepting the name of the new layer
+
+	GString				*tmp_gstring;			// Temporary GString
+
+
+	// Initialise various things
+	tmp_gstring = g_string_new(NULL);
+
+	// * Display a dialog box asking for the new name of the slide *
+
+	// Create the dialog window, and table to hold its children
+	slide_dialog = GTK_DIALOG(gtk_dialog_new_with_buttons("Edit slide name", GTK_WINDOW(main_window), GTK_DIALOG_MODAL, GTK_STOCK_OK, GTK_RESPONSE_ACCEPT, GTK_STOCK_CANCEL, GTK_RESPONSE_REJECT, NULL));
+	dialog_table = gtk_table_new(8, 2, FALSE);
+	gtk_box_pack_start(GTK_BOX(slide_dialog->vbox), GTK_WIDGET(dialog_table), FALSE, FALSE, 0);
+
+	// Create the label asking for the new layer name
+	name_label = gtk_label_new("Slide name: ");
+	gtk_misc_set_alignment(GTK_MISC(name_label), 0, 0.5);
+	gtk_table_attach_defaults(GTK_TABLE(dialog_table), GTK_WIDGET(name_label), 0, 1, 0, 1);
+
+	// Create the entry that accepts the new layer name
+	name_entry = gtk_entry_new();
+	gtk_entry_set_max_length(GTK_ENTRY(name_entry), 50);
+	if (NULL != ((slide *) current_slide->data)->name)
+	{
+		// Display the present name in the text field
+		gtk_entry_set_text(GTK_ENTRY(name_entry), ((slide *) current_slide->data)->name->str);
+	} else
+	{
+		// No present name, so just use the slide's position
+		g_string_printf(tmp_gstring, "Slide %u", g_list_position(slides, current_slide) + 1);
+		gtk_entry_set_text(GTK_ENTRY(name_entry), tmp_gstring->str);
+	}
+	gtk_table_attach_defaults(GTK_TABLE(dialog_table), GTK_WIDGET(name_entry), 1, 2, 0, 1);
+
+	// Run the dialog
+	gtk_widget_show_all(GTK_WIDGET(slide_dialog));
+	dialog_result = gtk_dialog_run(GTK_DIALOG(slide_dialog));
+
+	// Was the OK button pressed?
+	if (GTK_RESPONSE_ACCEPT != dialog_result)
+	{
+		// * The user cancelled the dialog *
+
+		// Destroy the dialog box then return
+		gtk_widget_destroy(GTK_WIDGET(slide_dialog));
+	}
+
+	// fixme4: We should validate the user input (slide name) here
+
+	// Set the new slide name
+	if (NULL == ((slide *) current_slide->data)->name)
+	{
+		((slide *) current_slide->data)->name = g_string_new(NULL);
+	}
+	g_string_printf(((slide *) current_slide->data)->name, "%s", gtk_entry_get_text(GTK_ENTRY(name_entry)));
+
+	// Destroy the dialog box
+	gtk_widget_destroy(GTK_WIDGET(slide_dialog));
+
+	// Recreate the film strip tooltips
+	create_tooltips();
+
+	// Free the resources allocated in this function
+	g_string_free(tmp_gstring, TRUE);
+}
+
+
 /* 
  * History
  * +++++++
  * 
  * $Log$
+ * Revision 1.31  2006/06/10 15:38:41  vapour
+ * Committed the code for setting and unsetting slide names, which I'd somehow managed to miss committing before.
+ *
  * Revision 1.30  2006/06/10 15:35:26  vapour
  * The File -> New option no longer unloads the presently loaded project if the user cancels out.
  *
