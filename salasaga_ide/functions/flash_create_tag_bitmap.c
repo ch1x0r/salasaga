934a5e5d101fc9eb2c2e724e6f132e0168157609
Justin Clift <justin@salasaga.org>
2007-10-03 13:41:23 +0000

    Added initial untested code for converting slide layers to flash bitmap (jpeg2) data.

--- salasaga_ide/functions/flash_create_tag_bitmap.c
+++ salasaga_ide/functions/flash_create_tag_bitmap.c
@@ -37,34 +37,61 @@
 #include "../externs.h"
 
 
-GByteArray *flash_create_tag_bitmap(layer *layer_data)
+GByteArray *flash_create_tag_bitmap(layer_image *layer_data)
 {
 	// Local variables
-	GByteArray			*output_array;
+	gsize				buffer_size;
+	GError				*error = NULL;
+	gchar				*jpeg_buffer;
+	GByteArray			*output_buffer;
 	guint16				record_header;
 	guint16				tag_code;
 	guint16				tag_header;
 	guint16				tag_length;
+	gboolean			return_code_bool;
 
 
 	// Initialise variables
-	output_array = g_byte_array_new();
+	output_buffer = g_byte_array_new();
 	record_header = 0;
 
 	// Not sure if we should use SWF_TAG_DEFINE_BITS_JPEG2 or SWF_TAG_DEFINE_BITS_LOSSLESS
 	// Might start with SWF_TAG_DEFINE_BITS_JPEG2, as I think we already have jpeg encoding functions
 	tag_code = SWF_TAG_DEFINE_BITS_JPEG2 << 6;
 
-	// Fixme2: Needs to be written
+	// Jpeg encode the image data
+	return_code_bool = gdk_pixbuf_save_to_buffer(layer_data->image_data,	// Pointer to GDK-pixbuf data
+												 &jpeg_buffer,				// Output buffer
+												 &buffer_size,				// Size of output buffer
+												 "jpeg",					// Format to save data in
+												 &error,					// Error indicator
+												 "quality",					// Key string
+												 "100",						// Value string
+												 NULL);						// NULL terminated list of options
+	if (TRUE == return_code_bool)
+	{
+		// Something went wrong when creating the jpeg data, so display a warning
+		display_warning(error->message);
 
-	tag_length = 0;  // Fixme3: Needs to be figured out
+		// Free resources allocated in this function
+		g_error_free(error);
+
+		// Return to the calling function
+		return NULL;
+	}
+
+	// Add the image data to the output buffer
+	output_buffer = g_byte_array_append(output_buffer, (guint8 *) jpeg_buffer, buffer_size);
+
+	// Embed the length of the output buffer in the tag header
+	tag_length = output_buffer->len;
 	tag_header = tag_code | tag_length;
 
 	// Prepend the tag type and length
-	g_byte_array_prepend(output_array, (guint8 *) &tag_header, 2);
+	g_byte_array_prepend(output_buffer, (guint8 *) &tag_header, 2);
 
 	// Return the fully formed dictionary shape
-	return output_array;
+	return output_buffer;
 }
 
 
@@ -73,6 +100,9 @@ GByteArray *flash_create_tag_bitmap(layer *layer_data)
  * +++++++
  * 
  * $Log$
+ * Revision 1.3  2007/10/03 13:41:23  vapour
+ * Added initial untested code for converting slide layers to flash bitmap (jpeg2) data.
+ *
  * Revision 1.2  2007/10/01 13:18:54  vapour
  * Started giving thought to the initial stub swf bitmap definition function.
  *
