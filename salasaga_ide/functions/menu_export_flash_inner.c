443b0d23c8727e28b6ac6d8e074cc7fb736fccc4
Justin Clift <justin@salasaga.org>
2008-01-15 12:01:55 +0000

    Significantly reworked much of the swf generation pre-processing loops and array processing code.  Added a lot of comments and debugging output as well.

--- salasaga_ide/functions/menu_export_flash_inner.c
+++ salasaga_ide/functions/menu_export_flash_inner.c
@@ -37,8 +37,8 @@
 SWFMovie menu_export_flash_inner(SWFMovie this_movie)
 {
 	// Local variables
-	guint16				char_counter;				// Counter of the number of characters in the animation
 	gboolean			dictionary_shape_ok;		// Temporary value indicating if a dictionary shape was created ok or not
+	guint				frame_counter;				// Holds the number of frames
 	gint				highlight_box_width;		// Used while generating swf output for highlight boxes
 	gint				highlight_box_height;		// Used while generating swf output for highlight boxes
 	guint				layer_counter;				// Holds the number of layers
@@ -49,7 +49,9 @@ SWFMovie menu_export_flash_inner(SWFMovie this_movie)
 	GdkPixbuf			*resized_pixbuf;			// Temporary pixbuf used while scaling images
 	guint				slide_counter;				// Holds the number of slides
 	swf_frame_element	*swf_timing_array = NULL;	// Used to coordinate the actions in each frame
+	swf_frame_element 	this_frame;					// Points to frame information when looping
 	layer				*this_layer_data;			// Points to the data in the present layer
+	layer				*this_layer_info;			// Used to point to layer data when looping
 	slide				*this_slide_data;			// Points to the data in the present slide
 	guint				total_frames;				// The total number of frames in the animation
 
@@ -57,23 +59,27 @@ SWFMovie menu_export_flash_inner(SWFMovie this_movie)
 	SWFShape			highlight_box;				// Temporary swf shape used when constructing highlight boxes
 	SWFFillStyle		highlight_fill_style;		// Fill style used when constructing highlight boxes
 
-	guint				element_counter;
-	guint				element_max;
+	guint				element_number;
+	guint				frame_number;
 
+	gint				element_x_position_finish = 0;
 	gint				element_x_position_increment = 0;
 	gint				element_x_position_start = 0;
+	gint				element_y_position_finish = 0;
 	gint				element_y_position_increment = 0;
 	gint				element_y_position_start = 0;
 
-	guint				tmp_integer;				// Temporary integer
-
 
 	// Initialise variables
-	char_counter = 1;  // Character 0 is reserved in Flash, so we avoid it
 	total_frames = 0;
 
 	// Create the fill style used in highlight boxes
 	highlight_fill_style = newSWFSolidFillStyle(0x00, 0xff, 0x00, 0x40);
+	if (NULL == highlight_fill_style)
+	{
+		// Something went wrong creating the fill style, so we don't proceed with creating the swf
+		return NULL;
+	}
 
 
 // For now, this uses a two pass per slide approach
@@ -137,16 +143,10 @@ SWFMovie menu_export_flash_inner(SWFMovie this_movie)
 		{
 			this_layer_data = g_list_nth_data(this_slide_data->layers, layer_counter);
 
-			// Mark that the element needs to be shown on this frame
-			swf_timing_array[(layer_counter * max_frames) + this_layer_data->start_frame].add = TRUE;
-
-			// Mark that the element needs to be removed on this frame
-			swf_timing_array[(layer_counter * max_frames) + this_layer_data->finish_frame].remove = TRUE;
-
 			// Mark this element as not having a dictionary shape created yet
 			dictionary_shape_ok = FALSE;
 
-			// Calculate the starting positions, increments and other info for each frame of the layer
+			// Create the dictionary shape for this layer
 			switch (this_layer_data->object_type)
 			{
 				case TYPE_GDK_PIXBUF:
@@ -161,21 +161,11 @@ SWFMovie menu_export_flash_inner(SWFMovie this_movie)
 						display_warning("Error ED90: Something went wrong when creating the dictionary shape for this image layer");
 
 						// * We don't want to process this layer any further, so we skip adding it to the dictionary
+						break;
 
-					} else
-					{
-						// Creation of shape went ok, so continue
-						swf_timing_array[(layer_counter * max_frames) + this_layer_data->start_frame].layer_type = TYPE_GDK_PIXBUF;
-						swf_timing_array[(layer_counter * max_frames) + this_layer_data->start_frame].layer_info = this_layer_data;
-						element_x_position_start = ((layer_image *) this_layer_data->object_data)->x_offset_start;
-						element_y_position_start = ((layer_image *) this_layer_data->object_data)->y_offset_start;
-						element_x_position_increment = (((layer_image *) this_layer_data->object_data)->x_offset_finish - ((layer_image *) this_layer_data->object_data)->x_offset_start) / (this_layer_data->finish_frame - this_layer_data->start_frame);
-						element_y_position_increment = (((layer_image *) this_layer_data->object_data)->y_offset_finish - ((layer_image *) this_layer_data->object_data)->y_offset_start) / (this_layer_data->finish_frame - this_layer_data->start_frame);
-
-						// fixme2: Create the dictionary shape for this layer
-					
-						// fixme2: Add it to the display list
 					}
+
+					// fixme2: Create the dictionary shape for this layer
 
 					break;
 
@@ -183,15 +173,13 @@ SWFMovie menu_export_flash_inner(SWFMovie this_movie)
 
 					// * We're processing a highlight layer *
 
-					// Set up the required information in the layer element array
-					swf_timing_array[(layer_counter * max_frames) + this_layer_data->start_frame].layer_type = TYPE_HIGHLIGHT;
-					swf_timing_array[(layer_counter * max_frames) + this_layer_data->start_frame].layer_info = this_layer_data;
-					element_x_position_start = ((layer_highlight *) this_layer_data->object_data)->x_offset_start;
-					element_y_position_start = ((layer_highlight *) this_layer_data->object_data)->y_offset_start;
-					element_x_position_increment = (((layer_highlight *) this_layer_data->object_data)->x_offset_finish - ((layer_highlight *) this_layer_data->object_data)->x_offset_start) / (this_layer_data->finish_frame - this_layer_data->start_frame);  // fixme3: This MAY need to be scaled
-					element_y_position_increment = (((layer_highlight *) this_layer_data->object_data)->y_offset_finish - ((layer_highlight *) this_layer_data->object_data)->y_offset_start) / (this_layer_data->finish_frame - this_layer_data->start_frame);  // fixme3: This MAY need to be scaled
-
-					// * Create the dictionary shape for this layer *
+					// Create the initial empty shape
+					highlight_box = newSWFShape();
+					if (NULL == highlight_box)
+					{
+						// Something went wrong when creating the empty shape, so we skip this layer
+						break;
+					}
 
 					// Set the semi-transparent green fill for the highlight box
 					SWFShape_setRightFillStyle(highlight_box, highlight_fill_style);
@@ -219,37 +207,15 @@ SWFMovie menu_export_flash_inner(SWFMovie this_movie)
 
 					// We're processing a mouse layer
 
-					break;  // The code for this layer type hasn't been written yet
-
-					swf_timing_array[(layer_counter * max_frames) + this_layer_data->start_frame].layer_type = TYPE_MOUSE_CURSOR;
-					swf_timing_array[(layer_counter * max_frames) + this_layer_data->start_frame].layer_info = this_layer_data;
-					element_x_position_start = ((layer_mouse *) this_layer_data->object_data)->x_offset_start;
-					element_y_position_start = ((layer_mouse *) this_layer_data->object_data)->y_offset_start;
-					element_x_position_increment = (((layer_mouse *) this_layer_data->object_data)->x_offset_finish - ((layer_mouse *) this_layer_data->object_data)->x_offset_start) / (this_layer_data->finish_frame - this_layer_data->start_frame);
-					element_y_position_increment = (((layer_mouse *) this_layer_data->object_data)->y_offset_finish - ((layer_mouse *) this_layer_data->object_data)->y_offset_start) / (this_layer_data->finish_frame - this_layer_data->start_frame);
-
 					// fixme2: Create the dictionary shape for this layer
 					
-					// fixme2: Add it to the display list
-
 					break;
 
 				case TYPE_TEXT:
 
 					// We're processing a text layer
 
-					break;  // The code for this layer type hasn't been written yet
-
-					swf_timing_array[(layer_counter * max_frames) + this_layer_data->start_frame].layer_type = TYPE_TEXT;
-					swf_timing_array[(layer_counter * max_frames) + this_layer_data->start_frame].layer_info = this_layer_data;
-					element_x_position_start = ((layer_text *) this_layer_data->object_data)->x_offset_start;
-					element_y_position_start = ((layer_text *) this_layer_data->object_data)->y_offset_start;
-					element_x_position_increment = (((layer_text *) this_layer_data->object_data)->x_offset_finish - ((layer_text *) this_layer_data->object_data)->x_offset_start) / (this_layer_data->finish_frame - this_layer_data->start_frame);
-					element_y_position_increment = (((layer_text *) this_layer_data->object_data)->y_offset_finish - ((layer_text *) this_layer_data->object_data)->y_offset_start) / (this_layer_data->finish_frame - this_layer_data->start_frame);
-
 					// fixme2: Create the dictionary shape for this layer
-					
-					// fixme2: Add it to the display list
 
 					break;
 
@@ -259,75 +225,178 @@ SWFMovie menu_export_flash_inner(SWFMovie this_movie)
 					break;
 			}
 
-			// If the creation of the dictionary shape didn't work, we shouldn't add this layer to the list for processing
-			if (TRUE != dictionary_shape_ok)
+			// If the creation of the dictionary shape worked, we add this layer to the list for processing
+			if (TRUE == dictionary_shape_ok)
 			{
 				// Process the element array, setting flags and info as required for this layer
-				element_max = this_layer_data->finish_frame;
+				element_x_position_start = ((layer_highlight *) this_layer_data->object_data)->x_offset_start;
+				element_x_position_finish = ((layer_highlight *) this_layer_data->object_data)->x_offset_finish;
+				element_y_position_start = ((layer_highlight *) this_layer_data->object_data)->y_offset_start;
+				element_y_position_finish = ((layer_highlight *) this_layer_data->object_data)->y_offset_finish;
+
+				// If the layer moves, flag this and calculate the increment in each direction
+				if ((element_x_position_start != element_x_position_finish) || (element_y_position_start != element_y_position_finish))
+				{
+					swf_timing_array[(layer_counter * max_frames) + this_layer_data->start_frame].is_moving = TRUE;
+					element_x_position_increment = (((layer_highlight *) this_layer_data->object_data)->x_offset_finish - ((layer_highlight *) this_layer_data->object_data)->x_offset_start) / (this_layer_data->finish_frame - this_layer_data->start_frame);  // fixme3: This MAY need to be scaled
+					element_y_position_increment = (((layer_highlight *) this_layer_data->object_data)->y_offset_finish - ((layer_highlight *) this_layer_data->object_data)->y_offset_start) / (this_layer_data->finish_frame - this_layer_data->start_frame);  // fixme3: This MAY need to be scaled
+				}
+
+				// Indicate on which frame the element should be displayed
+				frame_number = (layer_counter * max_frames) + this_layer_data->start_frame;
+				swf_timing_array[frame_number].add = TRUE;
+
+				// Displaying debugging info if requested
+				if (debug_level)
+				{
+					printf("Setting ADD value for layer %u in swf element %u\n", layer_counter, frame_number);
+				}
+
+				// Indicate on which frame the element should be removed from display
+				frame_number = (layer_counter * max_frames) + this_layer_data->finish_frame;
+				swf_timing_array[frame_number].remove = TRUE;
+
+				// Displaying debugging info if requested
+				if (debug_level)
+				{
+					printf("Setting REMOVE value for layer %u in swf element %u\n", layer_counter, frame_number);
+				}
 
 				// Re-initialise the position counter for each shape
 				position_counter = 0;
-				for (element_counter = this_layer_data->start_frame; element_counter <= element_max; element_counter++)
+				for (frame_number = this_layer_data->start_frame; frame_number <= this_layer_data->finish_frame; frame_number++)
 				{
-					// Mark that the element should be processed on this frame
-					swf_timing_array[(layer_counter * max_frames) + element_counter].action_this = TRUE;
+					element_number = (layer_counter * max_frames) + frame_number;
+					this_frame = swf_timing_array[element_number];
 
-					// Store the x and y positions for each frame
-					swf_timing_array[(layer_counter * max_frames) + element_counter].x_position = element_x_position_start + (position_counter * element_x_position_increment);
-					swf_timing_array[(layer_counter * max_frames) + element_counter].y_position = element_y_position_start + (position_counter * element_y_position_increment);
-					swf_timing_array[(layer_counter * max_frames) + element_counter].is_moving = TRUE;
-					position_counter++;
+					// Display debugging info if requested
+					if (debug_level)
+					{
+						printf("Setting values inside element # %u\n", element_number);
+					}
+
+					// Indicate the element should be processed on this frame
+					this_frame.action_this = TRUE;
+
+					// Ensure the appropriate layer data can be found by later code 
+					this_frame.layer_info = this_layer_data;
+
+					// If the layer moves, store the x and y positions for each frame
+					if (TRUE == swf_timing_array[(layer_counter * max_frames) + this_layer_data->start_frame].is_moving)
+					{
+						this_frame.is_moving = TRUE;
+						this_frame.x_position = element_x_position_start + (position_counter * element_x_position_increment);
+						this_frame.y_position = element_y_position_start + (position_counter * element_y_position_increment);
+						position_counter++;
+					}
 
 					// Store the opacity setting for each frame
 					// fixme2: Still need to calculate properly rather than hard code to 100% for the moment
-					swf_timing_array[(layer_counter * max_frames) + element_counter].opacity = 65535;
+					this_frame.opacity = 65535;
 				}
 
 			}
 		}
 
+		// Debugging output, displaying what we have in the pre-processing element array thus far
+		if (debug_level)
+		{
+			for (frame_counter = 0; frame_counter < max_frames; frame_counter++)  // This loops _frame_ number of times
+			{
+				for (layer_counter = 0; layer_counter < num_layers; layer_counter++)  // This loops _num_layers_ of times
+				{
+					frame_number = (frame_counter * num_layers) + layer_counter;
+					this_frame = swf_timing_array[frame_number];
+
+					printf("Frame number %u:\n", frame_number);
+
+					if (TRUE == this_frame.action_this)
+						printf("Action frame: ON\t");
+					else
+						printf("Action frame: OFF\t");
+
+					if (NULL != this_frame.layer_info)
+						printf("Layer info: SET\t");
+					else
+						printf("Layer info: NULL\t");
+
+					if (TRUE == this_frame.add)
+						printf("Add frame: ON\t");
+					else
+						printf("Add frame: OFF\t");
+
+					if (TRUE == this_frame.remove)
+						printf("Remove frame: ON\t");
+					else
+						printf("Remove frame: OFF\t");
+
+					if (TRUE == this_frame.is_moving)
+					{
+						printf("Move frame: ON\tX position: %u\tY position %u\t", this_frame.x_position, this_frame.y_position);
+					}
+					else
+						printf("Move frame: OFF\t");
+
+					printf("\n\n");
+				}
+			}
+		}
+
 		// * After all of the layers have been pre-processed we have an array with the per frame info of what should be *
 		// * where in the output swf, plus we also have the swf dictionary created and ready to use                     *
-		for (tmp_integer = 0; tmp_integer < max_frames; tmp_integer++)  // This loops _frame_ number of times
+		for (frame_counter = 0; frame_counter < max_frames; frame_counter++)  // This loops _frame_ number of times
 		{
-			for (element_counter = 0; element_counter < num_layers; element_counter++)  // This loops _num_layers_ of times
+			for (layer_counter = 0; layer_counter < num_layers; layer_counter++)  // This loops _num_layers_ of times
 			{
 				// For each frame, access all of the layers then move to the next frame
-				if (TRUE == swf_timing_array[(tmp_integer * num_layers) + element_counter].action_this)
+				frame_number = (frame_counter * num_layers) + layer_counter;
+				this_frame = swf_timing_array[frame_number];
+
+				// Display debugging info if requested
+				if (debug_level)
+				{
+					printf("Processing swf element # %u\n", frame_number);
+				}
+
+				// Is this a frame in which some action needs to take place?
+				if (TRUE == this_frame.action_this)
 				{
 					// * There is something to be done in this frame for this layer *
 
-					if (TRUE == swf_timing_array[(tmp_integer * num_layers) + element_counter].add)
+					this_layer_info = this_frame.layer_info;
+
+					// Is this the frame in which the layer is added to the display?
+					if (TRUE == this_frame.add)
 					{
 						// * Add the character to the swf display list *
-						display_list_object = SWFMovie_add(this_movie, (SWFBlock) swf_timing_array[(tmp_integer * num_layers) + element_counter].layer_info->dictionary_shape);
+						display_list_object = SWFMovie_add(this_movie, (SWFBlock) this_layer_info->dictionary_shape);
 
 						// Store the display list object for future reference
-						swf_timing_array[(tmp_integer * num_layers) + element_counter].layer_info->display_list_item = display_list_object;
+						this_layer_info->display_list_item = display_list_object;
 					}
 
-					if (TRUE == swf_timing_array[(tmp_integer * num_layers) + element_counter].remove)
+					// Is this the frame in which the layer is removed from the display?
+					if (TRUE == this_frame.remove)
 					{
 						// * Remove the character from the swf display list *
 
 						// Get the appropriate display list object
-						guint start_frame = swf_timing_array[(tmp_integer * num_layers) + element_counter].layer_info->start_frame;
-						display_list_object = swf_timing_array[(tmp_integer * num_layers) + start_frame].layer_info->display_list_item;
+						display_list_object = this_layer_info->display_list_item;
 
 						// Remove the character from the display list
 						SWFDisplayItem_remove(display_list_object);
 					}
 
-					if (TRUE == swf_timing_array[(tmp_integer * num_layers) + element_counter].is_moving)
+					// Does the layer need to be moved/positioned in this frame?
+					if (TRUE == this_frame.is_moving)
 					{
 						// * Adjust the x and y position of the character on the display list *
 
 						// Get the appropriate display list object
-						guint start_frame = swf_timing_array[(tmp_integer * num_layers) + element_counter].layer_info->start_frame;
-						display_list_object = swf_timing_array[(tmp_integer * num_layers) + start_frame].layer_info->display_list_item;
+						display_list_object = this_layer_info->display_list_item;
 
 						// (Re-)position the object
-						SWFDisplayItem_moveTo(display_list_object, swf_timing_array[(tmp_integer * num_layers) + element_counter].x_position, swf_timing_array[(tmp_integer * num_layers) + element_counter].y_position);
+						SWFDisplayItem_moveTo(display_list_object, this_frame.x_position, this_frame.y_position);
 					}
 				}
 			}
@@ -353,6 +422,9 @@ SWFMovie menu_export_flash_inner(SWFMovie this_movie)
  * +++++++
  * 
  * $Log$
+ * Revision 1.22  2008/01/15 12:01:55  vapour
+ * Significantly reworked much of the swf generation pre-processing loops and array processing code.  Added a lot of comments and debugging output as well.
+ *
  * Revision 1.21  2008/01/13 12:24:31  vapour
  * Adjusted the size of the highlight box to be closer to the real desired size.  Still needs to be scaled though.
  *
