7f394f48e60a051272d5ce7f0e155e1457ed8ecd
Justin Clift <justin@salasaga.org>
2008-01-21 10:50:37 +0000

    Simplified things, and delegated creation of new timeline duration images to the main timeline duration image regen function.

--- salasaga_ide/functions/layer_edit.c
+++ salasaga_ide/functions/layer_edit.c
@@ -63,33 +63,26 @@
 void layer_edit(void)
 {
 	// Local variables
-	GdkPixbuf			*layer_pixbuf;				// Pointer used when creating duration images for layers
 	GList				*layer_pointer;				// Points to the layers in the selected slide
-	GtkListStore			*list_pointer;				//
+	GtkListStore		*list_pointer;				//
 	GtkWidget			*list_widget;				// Points to the timeline widget
-	guint				num_layers;				// Number of layers
+	guint				num_layers;					// Number of layers
 	guint				selected_row;				// Holds the row that is selected
 	slide				*slide_data;				// Pointer to current slide data
 	gboolean			return_code;				// Did the edit dialog return ok?
 
-	guint				start_frame;				// Used when working out a layer's start frame
-	guint				finish_frame;				// Used when working out a layer's finish frame
-	gfloat				start_pixel;				// Starting slider pixel to fill in
-	gfloat				finish_pixel;				// Ending slider pixel to fill in
-	gfloat				pixel_width;				// Width of pixels to fill
+	GtkTextIter			text_start;					// The start position of the text buffer
+	GtkTextIter			text_end;					// The end position of the text buffer
 
-	GtkTextIter			text_start;				// The start position of the text buffer
-	GtkTextIter			text_end;				// The end position of the text buffer
-
-	GtkTreeViewColumn		*tmp_column;				// Temporary column
+	GtkTreeViewColumn	*tmp_column;				// Temporary column
 	layer_empty			*tmp_empty_ob;				// Temporary empty layer object
-	layer_highlight			*tmp_highlight_ob;			// Temporary highlight layer object
+	layer_highlight		*tmp_highlight_ob;			// Temporary highlight layer object
 	layer_image			*tmp_image_ob;				// Temporary image layer object
-	GtkTreeIter			*tmp_iter;				// Temporary iter
-	layer				*tmp_layer;				// Temporary layer
+	GtkTreeIter			*tmp_iter;					// Temporary iter
+	layer				*tmp_layer;					// Temporary layer
 	layer_mouse			*tmp_mouse_ob;				// Temporary mouse layer object
 	layer_text			*tmp_text_ob;				// Temporary text layer object
-	GtkTreePath			*tmp_path;				// Temporary path
+	GtkTreePath			*tmp_path;					// Temporary path
 
 
 	// If no project is loaded then don't run this function
@@ -101,7 +94,7 @@ void layer_edit(void)
 	}
 
 	// Initialise some variables
-	slide_data = ((slide *) current_slide->data);
+	slide_data = (slide *) current_slide->data;
 	layer_pointer = slide_data->layers;
 	list_pointer = slide_data->layer_store;
 	list_widget = slide_data->timeline_widget;
@@ -152,29 +145,12 @@ void layer_edit(void)
 				if (tmp_layer->finish_frame > slide_data->duration)
 				{
 					slide_data->duration = tmp_layer->finish_frame;
-
-					// Regenerate the duration images for the other layers as well!
-					regenerate_timeline_duration_images(slide_data);
 				}
 
-				// Work out the start and ending frames for this layer
-				start_frame = tmp_layer->start_frame;
-				finish_frame = tmp_layer->finish_frame;
-
-				// Calculate the duration of the layer for drawing inside the slider
-				start_pixel = 180 * ((gfloat) start_frame / (gfloat) slide_data->duration);
-				finish_pixel = 180 * ((gfloat) finish_frame / (gfloat) slide_data->duration);
-				pixel_width = finish_pixel - start_pixel;
-
-				// Create duration image
-				layer_pixbuf = NULL;
-				layer_pixbuf = create_timeline_slider(layer_pixbuf, 180, 20, start_pixel, pixel_width);
-				
 				// Update the slide list store with the new values
 				tmp_image_ob = (layer_image *) tmp_layer->object_data;
 				tmp_iter = tmp_layer->row_iter;
 				gtk_list_store_set(list_pointer, tmp_iter,
-							TIMELINE_DURATION, layer_pixbuf,
 							TIMELINE_X_OFF_START, tmp_image_ob->x_offset_start,
 							TIMELINE_Y_OFF_START, tmp_image_ob->y_offset_start,
 							TIMELINE_X_OFF_FINISH, tmp_image_ob->x_offset_finish,
@@ -194,29 +170,12 @@ void layer_edit(void)
 				if (tmp_layer->finish_frame > slide_data->duration)
 				{
 					slide_data->duration = tmp_layer->finish_frame;
-
-					// Regenerate the duration images for the other layers as well!
-					regenerate_timeline_duration_images(slide_data);
 				}
 
-				// Work out the start and ending frames for this layer
-				start_frame = tmp_layer->start_frame;
-				finish_frame = tmp_layer->finish_frame;
-
-				// Calculate the duration of the layer for drawing inside the slider
-				start_pixel = 180 * ((gfloat) start_frame / (gfloat) slide_data->duration);
-				finish_pixel = 180 * ((gfloat) finish_frame / (gfloat) slide_data->duration);
-				pixel_width = finish_pixel - start_pixel;
-
-				// Create duration image
-				layer_pixbuf = NULL;
-				layer_pixbuf = create_timeline_slider(layer_pixbuf, 180, 20, start_pixel, pixel_width);
-				
 				// Update the slide list store with the new values
 				tmp_mouse_ob = (layer_mouse *) tmp_layer->object_data;
 				tmp_iter = tmp_layer->row_iter;
 				gtk_list_store_set(list_pointer, tmp_iter,
-							TIMELINE_DURATION, layer_pixbuf,
 							TIMELINE_X_OFF_START, tmp_mouse_ob->x_offset_start,
 							TIMELINE_Y_OFF_START, tmp_mouse_ob->y_offset_start,
 							TIMELINE_X_OFF_FINISH, tmp_mouse_ob->x_offset_finish,
@@ -226,6 +185,7 @@ void layer_edit(void)
 			break;
 
 		case TYPE_TEXT:
+
 			// Open a dialog box for the user to edit the text layer values
 			return_code = display_dialog_text(tmp_layer, "Edit text layer");
 			if (TRUE == return_code)
@@ -236,31 +196,14 @@ void layer_edit(void)
 				if (tmp_layer->finish_frame > slide_data->duration)
 				{
 					slide_data->duration = tmp_layer->finish_frame;
-
-					// Regenerate the duration images for the other layers as well!
-					regenerate_timeline_duration_images(slide_data);
 				}
 
-				// Work out the start and ending frames for this layer
-				start_frame = tmp_layer->start_frame;
-				finish_frame = tmp_layer->finish_frame;
-
-				// Calculate the duration of the layer for drawing inside the slider
-				start_pixel = 180 * ((gfloat) start_frame / (gfloat) slide_data->duration);
-				finish_pixel = 180 * ((gfloat) finish_frame / (gfloat) slide_data->duration);
-				pixel_width = finish_pixel - start_pixel;
-
-				// Create duration image
-				layer_pixbuf = NULL;
-				layer_pixbuf = create_timeline_slider(layer_pixbuf, 180, 20, start_pixel, pixel_width);
-				
 				// Update the slide list store with the new values
 				tmp_text_ob = (layer_text *) tmp_layer->object_data;
 				tmp_iter = tmp_layer->row_iter;
 				gtk_text_buffer_get_bounds(((layer_text *) tmp_layer->object_data)->text_buffer, &text_start, &text_end);
 				gtk_list_store_set(list_pointer, tmp_iter,
 							TIMELINE_NAME, tmp_layer->name->str,
-							TIMELINE_DURATION, layer_pixbuf,
 							TIMELINE_X_OFF_START, tmp_text_ob->x_offset_start,
 							TIMELINE_Y_OFF_START, tmp_text_ob->y_offset_start,
 							TIMELINE_X_OFF_FINISH, tmp_text_ob->x_offset_finish,
@@ -270,6 +213,7 @@ void layer_edit(void)
 			break;
 
 		case TYPE_HIGHLIGHT:
+
 			// Open a dialog box for the user to edit the highlight layer values
 			return_code = display_dialog_highlight(tmp_layer, "Edit highlight layer");
 			if (TRUE == return_code)
@@ -280,29 +224,12 @@ void layer_edit(void)
 				if (tmp_layer->finish_frame > slide_data->duration)
 				{
 					slide_data->duration = tmp_layer->finish_frame;
-
-					// Regenerate the duration images for the other layers as well!
-					regenerate_timeline_duration_images(slide_data);
 				}
 
-				// Work out the start and ending frames for this layer
-				start_frame = tmp_layer->start_frame;
-				finish_frame = tmp_layer->finish_frame;
-
-				// Calculate the duration of the layer for drawing inside the slider
-				start_pixel = 180 * ((gfloat) start_frame / (gfloat) slide_data->duration);
-				finish_pixel = 180 * ((gfloat) finish_frame / (gfloat) slide_data->duration);
-				pixel_width = finish_pixel - start_pixel;
-
-				// Create duration image
-				layer_pixbuf = NULL;
-				layer_pixbuf = create_timeline_slider(layer_pixbuf, 180, 20, start_pixel, pixel_width);
-				
 				// Update the slide list store with the new values
 				tmp_highlight_ob = (layer_highlight *) tmp_layer->object_data;
 				tmp_iter = tmp_layer->row_iter;
 				gtk_list_store_set(list_pointer, tmp_iter,
-							TIMELINE_DURATION, layer_pixbuf,
 							TIMELINE_X_OFF_START, tmp_highlight_ob->x_offset_start,
 							TIMELINE_Y_OFF_START, tmp_highlight_ob->y_offset_start,
 							TIMELINE_X_OFF_FINISH, tmp_highlight_ob->x_offset_finish,
@@ -315,6 +242,9 @@ void layer_edit(void)
 			display_warning("Error ED34: Unknown layer type\n");
 			return;
 	}
+
+	// Regenerate the timeline duration images for all layers in this slide
+	regenerate_timeline_duration_images(slide_data);
 
 	// Redraw the workspace
 	draw_workspace();
@@ -329,6 +259,9 @@ void layer_edit(void)
  * +++++++
  * 
  * $Log$
+ * Revision 1.5  2008/01/21 10:50:37  vapour
+ * Simplified things, and delegated creation of new timeline duration images to the main timeline duration image regen function.
+ *
  * Revision 1.4  2008/01/19 06:40:29  vapour
  * Tweaked some error messages for clarity.
  *
