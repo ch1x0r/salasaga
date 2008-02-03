c18492eb6fe7dbaa2946bcd44af0617f87fb07d5
Justin Clift <justin@salasaga.org>
2008-02-03 00:45:54 +0000

    Respaced columns to line up on my monitor.  (Now using a monospace font, so hoping things won't go out of line so much.)

--- salasaga_ide/functions/create_menu_bar.c
+++ salasaga_ide/functions/create_menu_bar.c
@@ -92,50 +92,50 @@ void create_menu_bar()
 	//
 
 	// Local variables
-	GtkAccelGroup			*accel_group;				// Holds the accelerator (shortcut) keys
+	GtkAccelGroup			*accel_group;			// Holds the accelerator (shortcut) keys
 	static GtkItemFactoryEntry	menu_items[] =
 	{
-		{"/_File",			NULL,			NULL,				0,	"<Branch>"},
-		{"/File/_New",			"<control>N",		menu_file_new,			0,	"<Item>"},
-		{"/File/_Open",			"<control>O",		menu_file_open,			0,	"<Item>"},
-		{"/File/_Save",			"<control>S",		menu_file_save,			0,	"<Item>"},
-		{"/File/Sep",			NULL,			NULL,				0,	"<Separator>"},
-		{"/File/_Quit",			"<control>Q",		save_preferences_and_exit,	0,	"<Item>"},
+		{"/_File",						NULL,					NULL,							0,	"<Branch>"},
+		{"/File/_New",					"<control>N",			menu_file_new,					0,	"<Item>"},
+		{"/File/_Open",					"<control>O",			menu_file_open,					0,	"<Item>"},
+		{"/File/_Save",					"<control>S",			menu_file_save,					0,	"<Item>"},
+		{"/File/Sep",					NULL,					NULL,							0,	"<Separator>"},
+		{"/File/_Quit",					"<control>Q",			save_preferences_and_exit,		0,	"<Item>"},
 
-		{"/_Edit",			NULL,			NULL,				0,	"<Branch>"},
-		{"/Edit/_Preferences",		NULL,			menu_edit_preferences,		0,	"<Item>"},
+		{"/_Edit",						NULL,					NULL,							0,	"<Branch>"},
+		{"/Edit/_Preferences",			NULL,					menu_edit_preferences,			0,	"<Item>"},
 
-		{"/_Screenshots",		NULL,			NULL,				0,	"<Branch>"},
-		{"/Screenshots/_Capture",	NULL,			menu_screenshots_capture,	0,	"<Item>"},
-		{"/Screenshots/_Import",	NULL,			menu_screenshots_import,	0,	"<Item>"},
+		{"/_Screenshots",				NULL,					NULL,							0,	"<Branch>"},
+		{"/Screenshots/_Capture",		NULL,					menu_screenshots_capture,		0,	"<Item>"},
+		{"/Screenshots/_Import",		NULL,					menu_screenshots_import,		0,	"<Item>"},
 
-		{"/Sl_ide",			NULL,			NULL,				0,	"<Branch>"},
-		{"/Slide/_Insert",		NULL,			slide_insert,			0,	"<Item>"},
-		{"/Slide/_Delete",		NULL,			slide_delete,			0,	"<Item>"},
-		{"/Slide/Move _up",		NULL,			slide_move_up,			0,	"<Item>"},
-		{"/Slide/Move dow_n",		NULL,			slide_move_down,		0,	"<Item>"},
-		{"/Slide/Move to _top",		NULL,			slide_move_top,			0,	"<Item>"},
-		{"/Slide/Move to _bottom",	NULL,			slide_move_bottom,		0,	"<Item>"},
-		{"/Slide/_Edit slide name",	NULL,			slide_name_set,			0,	"<Item>"},
-		{"/Slide/_Reset slide name",	NULL,			slide_name_reset,		0,	"<Item>"},
+		{"/Sl_ide",						NULL,					NULL,							0,	"<Branch>"},
+		{"/Slide/_Insert",				NULL,					slide_insert,					0,	"<Item>"},
+		{"/Slide/_Delete",				NULL,					slide_delete,					0,	"<Item>"},
+		{"/Slide/Move _up",				NULL,					slide_move_up,					0,	"<Item>"},
+		{"/Slide/Move dow_n",			NULL,					slide_move_down,				0,	"<Item>"},
+		{"/Slide/Move to _top",			NULL,					slide_move_top,					0,	"<Item>"},
+		{"/Slide/Move to _bottom",		NULL,					slide_move_bottom,				0,	"<Item>"},
+		{"/Slide/_Edit slide name",		NULL,					slide_name_set,					0,	"<Item>"},
+		{"/Slide/_Reset slide name",	NULL,					slide_name_reset,				0,	"<Item>"},
 
-		{"/_Layer",			NULL,			NULL,				0,	"<Branch>"},
-		{"/Layer/_Edit",		NULL,			layer_edit,			0,	"<Item>"},
-		{"/Layer/_Delete",		NULL,			NULL,				0,	"<Item>"},
-		{"/Layer/Add _Text",		NULL,			layer_new_text,			0,	"<Item>"},
-		{"/Layer/Add _Highlight",	NULL,			layer_new_highlight,		0,	"<Item>"},
-		{"/Layer/Add _Image",		NULL,			layer_new_image,		0,	"<Item>"},
+		{"/_Layer",						NULL,					NULL,							0,	"<Branch>"},
+		{"/Layer/_Edit",				NULL,					layer_edit,						0,	"<Item>"},
+		{"/Layer/_Delete",				NULL,					NULL,							0,	"<Item>"},
+		{"/Layer/Add _Text",			NULL,					layer_new_text,					0,	"<Item>"},
+		{"/Layer/Add _Highlight",		NULL,					layer_new_highlight,			0,	"<Item>"},
+		{"/Layer/Add _Image",			NULL,					layer_new_image,				0,	"<Item>"},
 
-		{"/E_xport",			NULL,			NULL,				0,	"<Branch>"},
+		{"/E_xport",					NULL,					NULL,							0,	"<Branch>"},
 		{"/Export/_Flash Animation",	"<control><shift>F",	menu_export_flash_animation,	0,	"<Item>"},
-		{"/Export/_SVG Animation",	"<control><shift>S",	menu_export_svg_animation,	0,	"<Item>"},
+		{"/Export/_SVG Animation",		"<control><shift>S",	menu_export_svg_animation,		0,	"<Item>"},
 
-		{"/_Help",			NULL,			NULL,				0,	"<LastBranch>"},
-		{"/_Help/_About",		NULL,			menu_help_about,		0,	"<Item>"	},
-		{"/_Help/Sep",			NULL,			NULL,				0,	"<Separator>"},
-		{"/_Help/_Register",		NULL,			menu_help_register,		0,	"<Item>"	},
-		{"/_Help/_Survey",		NULL,			menu_help_survey,		0,	"<Item>"	},
-		{"/_Help/_Website",		NULL,			menu_help_website,		0,	"<Item>"	}
+		{"/_Help",						NULL,					NULL,							0,	"<LastBranch>"},
+		{"/_Help/_About",				NULL,					menu_help_about,				0,	"<Item>"	},
+		{"/_Help/Sep",					NULL,					NULL,							0,	"<Separator>"},
+		{"/_Help/_Register",			NULL,					menu_help_register,				0,	"<Item>"	},
+		{"/_Help/_Survey",				NULL,					menu_help_survey,				0,	"<Item>"	},
+		{"/_Help/_Website",				NULL,					menu_help_website,				0,	"<Item>"	}
 	};  // The menu structure
 	static gint			num_items = sizeof(menu_items) / sizeof(menu_items[0]);	// The number of menu items
 
@@ -164,6 +164,9 @@ void create_menu_bar()
  * +++++++
  * 
  * $Log$
+ * Revision 1.5  2008/02/03 00:45:54  vapour
+ * Respaced columns to line up on my monitor.  (Now using a monospace font, so hoping things won't go out of line so much.)
+ *
  * Revision 1.4  2008/01/15 16:19:04  vapour
  * Updated copyright notice to include 2008.
  *
