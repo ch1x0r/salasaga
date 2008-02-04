ace9af2f67b83d697da93baa22a73fca9b699c4d
Justin Clift <justin@salasaga.org>
2008-02-04 10:33:29 +0000

    Removed the code that went into the new Project Properties function.

--- salasaga_ide/functions/menu_edit_preferences.c
+++ salasaga_ide/functions/menu_edit_preferences.c
@@ -53,22 +53,15 @@ void menu_edit_preferences(void)
 {
 	// Local variables
 	GtkDialog			*main_dialog;						// Widget for the main dialog
-	GtkWidget			*main_notebook;						// Widget for the main notebook
 	GtkWidget			*app_dialog_table;					// Table used for neat layout of the labels and fields in application preferences
-	GtkWidget			*proj_dialog_table;					// Table used for neat layout of the labels and fields in project preferences
-	GtkWidget			*app_dialog_label;					// Tab text for the application tab of the dialog box
-	GtkWidget			*proj_dialog_label;					// Tab text for the project tab of the dialog box
 	gint				app_row_counter;					// Used when building the application preferences dialog box
-	gint				proj_row_counter;					// Used when building the project preferences dialog box
 	gint				dialog_result;						// Catches the return code from the dialog box
-
-	// * Application variables *
 
 	GtkWidget			*label_default_project_folder;		// Default Project Folder
 	GtkWidget			*button_default_project_folder;		//
 
-	GtkWidget			*label_screenshot_folder;		// Screenshot Folder
-	GtkWidget			*button_screenshot_folder;		//
+	GtkWidget			*label_screenshot_folder;			// Screenshot Folder
+	GtkWidget			*button_screenshot_folder;			//
 
 	GtkWidget			*label_default_output_folder;		// Default Output path
 	GtkWidget			*button_default_output_folder;		//
@@ -85,60 +78,29 @@ void menu_edit_preferences(void)
 	GtkWidget			*label_default_slide_length;		// Default Slide Length
 	GtkWidget			*button_default_slide_length;		//
 
-	GtkWidget			*label_preview_width;			// Preview Width
-	GtkWidget			*button_preview_width;			//
+	GtkWidget			*label_preview_width;				// Preview Width
+	GtkWidget			*button_preview_width;				//
 
-	GtkWidget			*label_icon_height;			// Icon Height
-	GtkWidget			*button_icon_height;			//
+	GtkWidget			*label_icon_height;					// Icon Height
+	GtkWidget			*button_icon_height;				//
 
-	GtkWidget			*label_default_zoom_level;		// Default Zoom Level
-	GtkWidget			*entry_default_zoom_level;		//
+	GtkWidget			*label_default_zoom_level;			// Default Zoom Level
+	GtkWidget			*entry_default_zoom_level;			//
 
-	GtkWidget			*label_default_bg_colour;		// Default background colour
-	GtkWidget			*button_default_bg_colour;		// Color button
+	GtkWidget			*label_default_bg_colour;			// Default background colour
+	GtkWidget			*button_default_bg_colour;			// Color button
 
-	GtkWidget			*label_scaling_quality;			// Scaling quality
-	GtkWidget			*button_scaling_quality;		//
-
-	// * Project variables *
-
-	GtkWidget			*label_project_name;			// Project Name
-	GtkWidget			*entry_project_name;			//
-
-	GtkWidget			*label_project_folder;			// Project Folder
-	GtkWidget			*button_project_folder;			//
-
-	GtkWidget			*label_output_folder;			// Output Folder
-	GtkWidget			*button_output_folder;			//
-
-	GtkWidget			*label_output_quality;			// Output Quality
-	GtkWidget			*button_output_quality;			//
-
-	GtkWidget			*label_slide_length;			// Slide Length
-	GtkWidget			*button_slide_length;			//
-
-	GtkWidget			*label_frames_per_second;		// Slide Length
-	GtkWidget			*button_frames_per_second;		//
+	GtkWidget			*label_scaling_quality;				// Scaling quality
+	GtkWidget			*button_scaling_quality;			//
 
 
 	// Initialise various things
 	app_row_counter = 0;
-	proj_row_counter = 0;
 
 	// Create the main dialog window
-	main_dialog = GTK_DIALOG(gtk_dialog_new_with_buttons("Preferences", GTK_WINDOW(main_window), GTK_DIALOG_MODAL, GTK_STOCK_OK, GTK_RESPONSE_ACCEPT, GTK_STOCK_CANCEL, GTK_RESPONSE_REJECT, NULL));
-	main_notebook = gtk_notebook_new();
-	gtk_notebook_set_tab_pos(GTK_NOTEBOOK(main_notebook), GTK_POS_TOP);
+	main_dialog = GTK_DIALOG(gtk_dialog_new_with_buttons("Application Preferences", GTK_WINDOW(main_window), GTK_DIALOG_MODAL, GTK_STOCK_OK, GTK_RESPONSE_ACCEPT, GTK_STOCK_CANCEL, GTK_RESPONSE_REJECT, NULL));
 	app_dialog_table = gtk_table_new(10, 3, FALSE);
-	app_dialog_label = gtk_label_new("General");
-	proj_dialog_table = gtk_table_new(10, 3, FALSE);
-	proj_dialog_label = gtk_label_new("This Project");
-	gtk_notebook_append_page(GTK_NOTEBOOK(main_notebook), app_dialog_table, app_dialog_label);
-	gtk_notebook_append_page(GTK_NOTEBOOK(main_notebook), proj_dialog_table, proj_dialog_label);
-	gtk_box_pack_start(GTK_BOX(main_dialog->vbox), GTK_WIDGET(main_notebook), FALSE, FALSE, 5);
-
-
-	// * Create Application preferences tab *
+	gtk_box_pack_start(GTK_BOX(main_dialog->vbox), GTK_WIDGET(app_dialog_table), FALSE, FALSE, 5);
 
 	// Default Project Folder
 	label_default_project_folder = gtk_label_new("Default Project Folder: ");
@@ -250,63 +212,6 @@ void menu_edit_preferences(void)
 	gtk_table_attach_defaults(GTK_TABLE(app_dialog_table), GTK_WIDGET(button_scaling_quality), 2, 3, app_row_counter, app_row_counter + 1);
 	app_row_counter = app_row_counter + 1;
 
-	// * Create Project Preferences tab *
-
-	// Project Name
-	label_project_name = gtk_label_new("Project Name: ");
-	gtk_misc_set_alignment(GTK_MISC(label_project_name), 0, 0.5);
-	gtk_table_attach_defaults(GTK_TABLE(proj_dialog_table), GTK_WIDGET(label_project_name), 0, 1, proj_row_counter, proj_row_counter + 1);
-	entry_project_name = gtk_entry_new();
-	gtk_entry_set_max_length(GTK_ENTRY(entry_project_name), 50);
-	gtk_entry_set_text(GTK_ENTRY(entry_project_name), (gchar *) project_name->str);
-	gtk_table_attach_defaults(GTK_TABLE(proj_dialog_table), GTK_WIDGET(entry_project_name), 2, 3, proj_row_counter, proj_row_counter + 1);
-	proj_row_counter = proj_row_counter + 1;
-
-	// Project Folder
-	label_project_folder = gtk_label_new("Project Folder: ");
-	gtk_misc_set_alignment(GTK_MISC(label_project_folder), 0, 0.5);
-	gtk_table_attach_defaults(GTK_TABLE(proj_dialog_table), GTK_WIDGET(label_project_folder), 0, 1, proj_row_counter, proj_row_counter + 1);
-	button_project_folder = gtk_file_chooser_button_new("Select the Project Folder", GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER);
-	gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(button_project_folder), project_folder->str);
-	gtk_table_attach_defaults(GTK_TABLE(proj_dialog_table), GTK_WIDGET(button_project_folder), 2, 3, proj_row_counter, proj_row_counter + 1);
-	proj_row_counter = proj_row_counter + 1;
-
-	// Output Folder
-	label_output_folder = gtk_label_new("Output Folder: ");
-	gtk_misc_set_alignment(GTK_MISC(label_output_folder), 0, 0.5);
-	gtk_table_attach_defaults(GTK_TABLE(proj_dialog_table), GTK_WIDGET(label_output_folder), 0, 1, proj_row_counter, proj_row_counter + 1);
-	button_output_folder = gtk_file_chooser_button_new("Select the Output Folder", GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER);
-	gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(button_output_folder), output_folder->str);
-	gtk_table_attach_defaults(GTK_TABLE(proj_dialog_table), GTK_WIDGET(button_output_folder), 2, 3, proj_row_counter, proj_row_counter + 1);
-	proj_row_counter = proj_row_counter + 1;
-
-	// Output Quality
-	label_output_quality = gtk_label_new("Output Quality: ");
-	gtk_misc_set_alignment(GTK_MISC(label_output_quality), 0, 0.5);
-	gtk_table_attach_defaults(GTK_TABLE(proj_dialog_table), GTK_WIDGET(label_output_quality), 0, 1, proj_row_counter, proj_row_counter + 1);
-	button_output_quality = gtk_spin_button_new_with_range(0, 9, 1);
-	gtk_spin_button_set_value(GTK_SPIN_BUTTON(button_output_quality), output_quality);
-	gtk_table_attach_defaults(GTK_TABLE(proj_dialog_table), GTK_WIDGET(button_output_quality), 2, 3, proj_row_counter, proj_row_counter + 1);
-	proj_row_counter = proj_row_counter + 1;
-
-	// Slide Length
-	label_slide_length = gtk_label_new("Default Slide Length: ");
-	gtk_misc_set_alignment(GTK_MISC(label_slide_length), 0, 0.5);
-	gtk_table_attach_defaults(GTK_TABLE(proj_dialog_table), GTK_WIDGET(label_slide_length), 0, 1, proj_row_counter, proj_row_counter + 1);
-	button_slide_length = gtk_spin_button_new_with_range(0, 1000, 10);
-	gtk_spin_button_set_value(GTK_SPIN_BUTTON(button_slide_length), slide_length);
-	gtk_table_attach_defaults(GTK_TABLE(proj_dialog_table), GTK_WIDGET(button_slide_length), 2, 3, proj_row_counter, proj_row_counter + 1);
-	proj_row_counter = proj_row_counter + 1;
-
-	// Frames per second
-	label_frames_per_second = gtk_label_new("Frames per second: ");
-	gtk_misc_set_alignment(GTK_MISC(label_frames_per_second), 0, 0.5);
-	gtk_table_attach_defaults(GTK_TABLE(proj_dialog_table), GTK_WIDGET(label_frames_per_second), 0, 1, proj_row_counter, proj_row_counter + 1);
-	button_frames_per_second = gtk_spin_button_new_with_range(0, 48, 1);
-	gtk_spin_button_set_value(GTK_SPIN_BUTTON(button_frames_per_second), frames_per_second);
-	gtk_table_attach_defaults(GTK_TABLE(proj_dialog_table), GTK_WIDGET(button_frames_per_second), 2, 3, proj_row_counter, proj_row_counter + 1);
-	proj_row_counter = proj_row_counter + 1;
-
 	// Set the dialog going
 	gtk_widget_show_all(GTK_WIDGET(main_dialog));
 	dialog_result = gtk_dialog_run(GTK_DIALOG(main_dialog));
@@ -314,9 +219,7 @@ void menu_edit_preferences(void)
 	// Was the OK button pressed?
 	if (GTK_RESPONSE_ACCEPT == dialog_result)
 	{
-		// * Yes, so update the project variables with their new values *
-
-		// ** Application Preferences **
+		// * Yes, so update the application variables with their new values *
 
 		// fixme3: No validation is done on this input yet.  To make a secure application, it really needs to be
 
@@ -357,26 +260,6 @@ void menu_edit_preferences(void)
 
 		// Scaling Quality
 		scaling_quality = (guint) gtk_spin_button_get_value(GTK_SPIN_BUTTON(button_scaling_quality));
-
-		// * Project Preferences *
-
-		// Project Name
-		project_name = g_string_assign(project_name, gtk_entry_get_text(GTK_ENTRY(entry_project_name)));
-
-		// Project Folder
-		project_folder = g_string_assign(project_folder, gtk_file_chooser_get_current_folder(GTK_FILE_CHOOSER(button_project_folder)));
-
-		// Output Folder
-		output_folder = g_string_assign(output_folder, gtk_file_chooser_get_current_folder(GTK_FILE_CHOOSER(button_output_folder)));
-
-		// Output Quality
-		output_quality = (guint) gtk_spin_button_get_value(GTK_SPIN_BUTTON(button_output_quality));
-
-		// Slide Length
-		slide_length = (guint) gtk_spin_button_get_value(GTK_SPIN_BUTTON(button_slide_length));
-
-		// Frames per second
-		frames_per_second = (guint) gtk_spin_button_get_value(GTK_SPIN_BUTTON(button_frames_per_second));
 	}
 
 	// Free up the memory allocated in this function
@@ -394,6 +277,9 @@ void menu_edit_preferences(void)
  * +++++++
  * 
  * $Log$
+ * Revision 1.3  2008/02/04 10:33:29  vapour
+ * Removed the code that went into the new Project Properties function.
+ *
  * Revision 1.2  2008/01/15 16:19:00  vapour
  * Updated copyright notice to include 2008.
  *
