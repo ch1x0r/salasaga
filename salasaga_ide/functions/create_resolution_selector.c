b89928db9e7849ecb594bbcfa715a817bd9b7425
Justin Clift <justin@salasaga.org>
2008-02-05 06:22:28 +0000

    Resolution selector widget now says pixels instead of px.

--- salasaga_ide/functions/create_resolution_selector.c
+++ salasaga_ide/functions/create_resolution_selector.c
@@ -39,7 +39,7 @@ GtkWidget *create_resolution_selector(ResolutionStructure *res_array, guint num_
 {
 	// Local variables
 	gboolean			match_found;				// Simple toggle used to indiate the requested resolution has been found
-	gint				match_at;				// If a resolution match was found, this contains where
+	gint				match_at;					// If a resolution match was found, this contains where
 	guint				res_counter;				// Used as a simple counter
 	GString				*res_string;				// Used for constructing resolution strings
 	GtkWidget			*res_widget;				// ComboBox widget that gets returned
@@ -68,14 +68,14 @@ GtkWidget *create_resolution_selector(ResolutionStructure *res_array, guint num_
 	// Construct the GtkComboBox from the supplied list of resolutions
 	for (res_counter = 0; res_counter < num_resolutions; res_counter++)
 	{
-		g_string_printf(res_string, "%ux%u px", (res_array + res_counter)->width, (res_array + res_counter)->height);
+		g_string_printf(res_string, "%ux%u pixels", (res_array + res_counter)->width, (res_array + res_counter)->height);
 		gtk_combo_box_append_text(GTK_COMBO_BOX(res_widget), res_string->str);
 	}
 
 	// If no match was found we add the requested resolution on to the end of the list
 	if (TRUE != match_found)
 	{
-		g_string_printf(res_string, "%ux%u px", initial_width, initial_height);
+		g_string_printf(res_string, "%ux%u pixels", initial_width, initial_height);
 		gtk_combo_box_append_text(GTK_COMBO_BOX(res_widget), res_string->str);
 		match_at = num_resolutions;  // Point to the new entry
 	}
@@ -96,6 +96,9 @@ GtkWidget *create_resolution_selector(ResolutionStructure *res_array, guint num_
  * +++++++
  * 
  * $Log$
+ * Revision 1.4  2008/02/05 06:22:28  vapour
+ * Resolution selector widget now says pixels instead of px.
+ *
  * Revision 1.3  2008/02/04 15:02:49  vapour
  *  + Removed unnecessary includes.
  *
