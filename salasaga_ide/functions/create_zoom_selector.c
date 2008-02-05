2dce052372837a13fb2b4a9b9740bea80027e45e
Justin Clift <justin@salasaga.org>
2008-02-05 10:44:25 +0000

    Added a new function to construct the zoom level widget.

---
+++ salasaga_ide/functions/create_zoom_selector.c
@@ -0,0 +1,93 @@
+/*
+ * $Id$
+ *
+ * Flame Project: Function to create the zoom level selector 
+ * 
+ * Copyright (C) 2008 Justin Clift <justin@postgresql.org>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ */
+
+
+// GTK includes
+#include <gtk/gtk.h>
+
+#ifdef _WIN32
+	// Windows only code
+	#include <windows.h>
+#endif
+
+// Flame Edit includes
+#include "../flame-types.h"
+#include "../externs.h"
+
+
+GtkWidget *create_zoom_selector(gchar *initial_zoom)
+{
+	// Local variables
+	gboolean			match_found;				// Simple toggle used to indiate the requested zoom has been found
+	gint				match_at;					// If a zoom match was found, this contains where
+	gint				num_zoom_items;				// The number of zoom options available
+	guint				zoom_counter;				// Used as a simple counter
+	gchar				*zoom_selector_array[] = { "1600%", "800%", "400%", "300%", "200%", "150%", "100%", "75%", "50%", "25%", "10%", "Fit to width" };  // The zoom options
+	GtkWidget			*zoom_widget;				// ComboBox widget that gets returned
+
+
+	// Initialise various things
+	match_found = FALSE;
+	match_at = -1;
+	zoom_widget = gtk_combo_box_new_text();
+	gtk_combo_box_set_focus_on_click(GTK_COMBO_BOX(zoom_widget), FALSE);
+
+	// Construct the zoom selector widget
+	num_zoom_items = sizeof(zoom_selector_array) / sizeof(zoom_selector_array[0]);
+	for (zoom_counter = 0; zoom_counter < num_zoom_items; zoom_counter++)
+	{
+		// Add the zoom option to the widget
+		gtk_combo_box_append_text(GTK_COMBO_BOX(zoom_widget), zoom_selector_array[zoom_counter]);
+
+		// Check if this zoom option is a match for the requested initial zoom
+		if (0 == g_ascii_strncasecmp(initial_zoom, zoom_selector_array[zoom_counter], 3))
+		{
+			match_found = TRUE;
+			match_at = zoom_counter;
+		}
+	}
+
+	// If no match was found we add the requested zoom to the end of the list
+	if (TRUE != match_found)
+	{
+		gtk_combo_box_append_text(GTK_COMBO_BOX(zoom_widget), initial_zoom);
+		match_at = num_zoom_items;  // Point to the new entry
+	}
+
+	// Select the chosen resolution as the default
+	gtk_combo_box_set_active(GTK_COMBO_BOX(zoom_widget), match_at);
+
+	// Return the resolution widget
+	return zoom_widget;
+}
+
+
+/*
+ * History
+ * +++++++
+ * 
+ * $Log$
+ * Revision 1.1  2008/02/05 10:44:25  vapour
+ * Added a new function to construct the zoom level widget.
+ *
+ */
---
+++ salasaga_ide/functions/create_zoom_selector.h
@@ -0,0 +1,47 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2007-2008 Justin Clift <justin@postgresql.org>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ */
+
+#ifndef CREATE_ZOOM_SELECTOR_H_
+#define CREATE_ZOOM_SELECTOR_H_
+
+// To keep C++ programs that include this header happy
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+GtkWidget *create_zoom_selector(gchar *initial_zoom);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif /*CREATE_ZOOM_SELECTOR_H_*/
+
+
+/* 
+ * History
+ * +++++++
+ * 
+ * $Log$
+ * Revision 1.1  2008/02/05 10:44:25  vapour
+ * Added a new function to construct the zoom level widget.
+ *
+ */
