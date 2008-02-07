20887c3185028da60c9ee272b71899e5d1f57893
Justin Clift <justin@salasaga.org>
2008-02-07 12:37:00 +0000

    Updated to only recalculate the zoom factor now when required.

--- salasaga_ide/functions/event_size_allocate_received.c
+++ salasaga_ide/functions/event_size_allocate_received.c
@@ -40,18 +40,33 @@ gint event_size_allocate_received(GtkWidget *widget, GdkEvent *event, gpointer d
 {
 	// Local variables
 	static gint			old_width = 0;
+	gint32				tmp_int;
+	GString				*tmp_string;
 
 
 	// Check if the width of the drawing area has changed
 	if (old_width != right_side->allocation.width)
 	{
-		// The width has been changed, so recalculate the zoom and redraw the drawing area
-		zoom_selector_changed(GTK_WIDGET(zoom_selector), NULL, (gpointer) NULL);
+		// * The width has been changed *
+		
+		// If the required zoom level is "Fit to width", then recalculate the zoom and redraw the drawing area
+		tmp_string = g_string_new(NULL);
+		g_string_printf(tmp_string, "%s", gtk_combo_box_get_active_text(GTK_COMBO_BOX(zoom_selector)));
+	
+		// Parse and store the new zoom level
+		tmp_int = g_ascii_strncasecmp(tmp_string->str, "F", 1);
+		if (0 == tmp_int)
+		{
+			zoom_selector_changed(GTK_WIDGET(zoom_selector), NULL, (gpointer) NULL);
+		}
+
 		old_width = right_side->allocation.width;
 
 		// Resize the drawing area so it draws properly
 		gtk_widget_set_size_request(GTK_WIDGET(main_drawing_area), working_width, working_height);
 
+		// Free the memory allocated in this function
+		g_string_free(tmp_string, TRUE);
 	}
 
 	// Return FALSE to continue event propagation
@@ -64,6 +79,9 @@ gint event_size_allocate_received(GtkWidget *widget, GdkEvent *event, gpointer d
  * +++++++
  * 
  * $Log$
+ * Revision 1.6  2008/02/07 12:37:00  vapour
+ * Updated to only recalculate the zoom factor now when required.
+ *
  * Revision 1.5  2008/02/04 16:50:03  vapour
  *  + Removed unnecessary includes.
  *
