2b5e56dac7da96e979004458bbe176dbc552b85a
Justin Clift <justin@salasaga.org>
2008-02-20 21:57:15 +0000

    Updated to validate all incoming input.

--- salasaga_ide/functions/display_dialog_text.c
+++ salasaga_ide/functions/display_dialog_text.c
@@ -33,17 +33,36 @@
 // Flame Edit includes
 #include "../flame-types.h"
 #include "../externs.h"
+#include "display_warning.h"
+#include "validate_value.h"
 
 
 gboolean display_dialog_text(layer *tmp_layer, gchar *dialog_title)
 {
 	// Local variables
+	guint				guint_val;					// Temporary guint value used for validation
+	guint				row_counter = 0;			// Used to count which row things are up to
 	GtkDialog			*text_dialog;				// Widget for the text dialog
 	GtkWidget			*text_table;				// Table used for neat layout of the dialog box
-	gint				dialog_result;				// Catches the return code from the dialog box
-	guint				row_counter = 0;			// Used to count which row things are up to
+	gboolean			useable_input;				// Used as a flag to indicate if all validation was successful
+	GString				*valid_ext_link;			// Receives the new external link once validated
+	GString				*valid_ext_link_win;		// Receives the new external link window once validated
+	GString				*valid_layer_name;			// Receives the new layer name once validated
+	guint				valid_finish_frame;			// Receives the new finish frame once validated
+	guint				valid_font_size;			// Receives the new font size once validated
+	guint				valid_start_frame;			// Receives the new start frame once validated
+	guint				valid_x_offset_finish;		// Receives the new finish frame x offset once validated
+	guint				valid_x_offset_start;		// Receives the new start frame x offset once validated
+	guint				valid_y_offset_finish;		// Receives the new finish frame y offset once validated
+	guint				valid_y_offset_start;		// Receives the new start frame y offset once validated
+	guint				*validated_guint;			// Receives known good guint values from the validation function
+	GString				*validated_string;			// Receives known good strings from the validation function
 
+	GtkTextBuffer		*text_buffer;				// Temporary text buffer the user words with 
+	GtkTextIter			text_end;					// End position of text buffer
 	GtkWidget			*text_frame;				// Frame to go around the text widget
+	GString				*text_gstring;				// Temporary text buffer
+	GtkTextIter			text_start;					// Start position of text buffer
 	GtkWidget			*text_view;					// Widget for accepting the new text data
 
 	GtkWidget			*name_label;				// Label widget
@@ -84,8 +103,11 @@ gboolean display_dialog_text(layer *tmp_layer, gchar *dialog_title)
 
 	// Initialise some things
 	tmp_text_ob = (layer_text *) tmp_layer->object_data;
+	valid_ext_link = g_string_new(NULL);
+	valid_ext_link_win = g_string_new(NULL);
+	valid_layer_name = g_string_new(NULL);
 
-	// * Pop open a dialog box asking the user for the details of the new text layer *
+	// * Open a dialog box asking the user for the details of the new text layer *
 
 	// Create the dialog window, and table to hold its children
 	text_dialog = GTK_DIALOG(gtk_dialog_new_with_buttons(dialog_title, GTK_WINDOW(main_window), GTK_DIALOG_MODAL, GTK_STOCK_OK, GTK_RESPONSE_ACCEPT, GTK_STOCK_CANCEL, GTK_RESPONSE_REJECT, NULL));
@@ -96,11 +118,19 @@ gboolean display_dialog_text(layer *tmp_layer, gchar *dialog_title)
 	text_frame = gtk_frame_new(NULL);
 	gtk_container_set_border_width(GTK_CONTAINER(text_frame), 2);
 	gtk_frame_set_shadow_type(GTK_FRAME(text_frame), GTK_SHADOW_OUT);
-	text_view = gtk_text_view_new_with_buffer(tmp_text_ob->text_buffer);
+	text_view = gtk_text_view_new();
 	gtk_widget_set_size_request(GTK_WIDGET(text_view), 0, 100);
 	gtk_container_add(GTK_CONTAINER(text_frame), text_view);
 	gtk_table_attach(GTK_TABLE(text_table), GTK_WIDGET(text_frame), 0, 2, row_counter, row_counter + 1, GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL, table_x_padding, table_y_padding);
 	row_counter = row_counter + 1;
+
+	// Copy the text string from the existing text buffer to a new, temporary one
+	// (Note - this is so we don't work directly with the text buffer, which would keep edits even if the user hits the Cancel button)
+	gtk_text_buffer_get_start_iter(tmp_text_ob->text_buffer, &text_start);
+	gtk_text_buffer_get_end_iter(tmp_text_ob->text_buffer, &text_end);
+	text_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+	text_gstring = g_string_new(gtk_text_buffer_get_slice(tmp_text_ob->text_buffer, &text_start, &text_end, TRUE));
+	gtk_text_buffer_set_text(GTK_TEXT_BUFFER(text_buffer), text_gstring->str, text_gstring->len);
 
 	// Create the label next to the color swatch
 	color_label = gtk_label_new("Color: ");
@@ -163,7 +193,7 @@ gboolean display_dialog_text(layer *tmp_layer, gchar *dialog_title)
 	gtk_table_attach(GTK_TABLE(text_table), GTK_WIDGET(font_label), 0, 1, row_counter, row_counter + 1, GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL, table_x_padding, table_y_padding);
 
 	// Create the entry that accepts the font size input
-	font_button = gtk_spin_button_new_with_range(0, 200, 10);  // fixme5: 200 was just plucked from the air
+	font_button = gtk_spin_button_new_with_range(0, valid_fields[FRAME_NUMBER].max_value, 10);
 	gtk_spin_button_set_value(GTK_SPIN_BUTTON(font_button), tmp_text_ob->font_size);
 	gtk_table_attach(GTK_TABLE(text_table), GTK_WIDGET(font_button), 1, 2, row_counter, row_counter + 1, GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL, table_x_padding, table_y_padding);
 	row_counter = row_counter + 1;
@@ -175,7 +205,7 @@ gboolean display_dialog_text(layer *tmp_layer, gchar *dialog_title)
 
 	// Create the entry that accepts the new layer name
 	name_entry = gtk_entry_new();
-	gtk_entry_set_max_length(GTK_ENTRY(name_entry), 50);
+	gtk_entry_set_max_length(GTK_ENTRY(name_entry), valid_fields[LAYER_NAME].max_value);
 	gtk_entry_set_text(GTK_ENTRY(name_entry), tmp_layer->name->str);
 	gtk_table_attach(GTK_TABLE(text_table), GTK_WIDGET(name_entry), 1, 2, row_counter, row_counter + 1, GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL, table_x_padding, table_y_padding);
 	row_counter = row_counter + 1;
@@ -186,7 +216,7 @@ gboolean display_dialog_text(layer *tmp_layer, gchar *dialog_title)
 	gtk_table_attach(GTK_TABLE(text_table), GTK_WIDGET(start_label), 0, 1, row_counter, row_counter + 1, GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL, table_x_padding, table_y_padding);
 
 	// Create the entry that accepts the starting frame input
-	start_button = gtk_spin_button_new_with_range(0, 200, 10);
+	start_button = gtk_spin_button_new_with_range(0, valid_fields[FRAME_NUMBER].max_value, 10);
 	gtk_spin_button_set_value(GTK_SPIN_BUTTON(start_button), tmp_layer->start_frame);
 	gtk_table_attach(GTK_TABLE(text_table), GTK_WIDGET(start_button), 1, 2, row_counter, row_counter + 1, GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL, table_x_padding, table_y_padding);
 	row_counter = row_counter + 1;
@@ -197,7 +227,7 @@ gboolean display_dialog_text(layer *tmp_layer, gchar *dialog_title)
 	gtk_table_attach(GTK_TABLE(text_table), GTK_WIDGET(finish_label), 0, 1, row_counter, row_counter + 1, GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL, table_x_padding, table_y_padding);
 
 	// Create the entry that accepts the finishing frame input
-	finish_button = gtk_spin_button_new_with_range(0, 1000, 10);
+	finish_button = gtk_spin_button_new_with_range(0, valid_fields[FRAME_NUMBER].max_value, 10);
 	gtk_spin_button_set_value(GTK_SPIN_BUTTON(finish_button), tmp_layer->finish_frame);
 	gtk_table_attach(GTK_TABLE(text_table), GTK_WIDGET(finish_button), 1, 2, row_counter, row_counter + 1, GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL, table_x_padding, table_y_padding);
 	row_counter = row_counter + 1;
@@ -209,7 +239,7 @@ gboolean display_dialog_text(layer *tmp_layer, gchar *dialog_title)
 
 	// Create the entry that accepts an external link
 	external_link_entry = gtk_entry_new();
-	gtk_entry_set_max_length(GTK_ENTRY(external_link_entry), 50);
+	gtk_entry_set_max_length(GTK_ENTRY(external_link_entry), valid_fields[EXTERNAL_LINK].max_value);
 	gtk_entry_set_text(GTK_ENTRY(external_link_entry), tmp_layer->external_link->str);
 	gtk_table_attach(GTK_TABLE(text_table), GTK_WIDGET(external_link_entry), 1, 2, row_counter, row_counter + 1, GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL, table_x_padding, table_y_padding);
 	row_counter = row_counter + 1;
@@ -221,44 +251,194 @@ gboolean display_dialog_text(layer *tmp_layer, gchar *dialog_title)
 
 	// Create the entry that accepts a text string for the window to open the external link in
 	external_link_win_entry = gtk_entry_new();
-	gtk_entry_set_max_length(GTK_ENTRY(external_link_win_entry), 50);
+	gtk_entry_set_max_length(GTK_ENTRY(external_link_win_entry), valid_fields[EXTERNAL_LINK_WINDOW].max_value);
 	gtk_entry_set_text(GTK_ENTRY(external_link_win_entry), tmp_layer->external_link_window->str);
 	gtk_table_attach(GTK_TABLE(text_table), GTK_WIDGET(external_link_win_entry), 1, 2, row_counter, row_counter + 1, GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL, table_x_padding, table_y_padding);
 	row_counter = row_counter + 1;
 
-	// Run the dialog
+	// Ensure everything will show
 	gtk_widget_show_all(GTK_WIDGET(text_dialog));
-	dialog_result = gtk_dialog_run(GTK_DIALOG(text_dialog));
 
-	// Was the OK button pressed?
-	if (GTK_RESPONSE_ACCEPT != dialog_result)
+	// Loop around until we have all valid values, or the user cancels out
+	validated_string = NULL;
+	do
 	{
-		// * The user cancelled the dialog *
+		// Display the dialog
+		if (GTK_RESPONSE_ACCEPT != gtk_dialog_run(GTK_DIALOG(text_dialog)))
+		{
+			// The dialog was cancelled, so destroy it and return to the caller
+			gtk_widget_destroy(GTK_WIDGET(text_dialog));
+			g_string_free(valid_ext_link, TRUE);
+			g_string_free(valid_ext_link_win, TRUE);
+			g_string_free(valid_layer_name, TRUE);
+			return FALSE;
+		}
 
-		// Destroy the dialog box then return
-		gtk_widget_destroy(GTK_WIDGET(text_dialog));
-		return FALSE;
-	}
+		// Reset the useable input flag
+		useable_input = TRUE;
 
-	// fixme4: We should validate the user input (layer name) here
+		// Retrieve the new starting frame x offset
+		guint_val = gtk_spin_button_get_value(GTK_SPIN_BUTTON(x_off_button_start));
+		validated_guint = validate_value(X_OFFSET, V_INT_UNSIGNED, &guint_val);
+		if (NULL == validated_guint)
+		{
+			display_warning("Error ED172: There was something wrong with the starting frame X offset value.  Please try again.");
+			useable_input = FALSE;
+		} else
+		{
+			valid_x_offset_start = *validated_guint;
+			g_free(validated_guint);
+		}
 
-	// Updated the layer in memory with the requested details
-	tmp_layer->x_offset_start = (gint) gtk_spin_button_get_value(GTK_SPIN_BUTTON(x_off_button_start));
-	tmp_layer->y_offset_start = (gint) gtk_spin_button_get_value(GTK_SPIN_BUTTON(y_off_button_start));
-	tmp_layer->x_offset_finish = (gint) gtk_spin_button_get_value(GTK_SPIN_BUTTON(x_off_button_finish));
-	tmp_layer->y_offset_finish = (gint) gtk_spin_button_get_value(GTK_SPIN_BUTTON(y_off_button_finish));
-	tmp_text_ob->font_size = (gint) gtk_spin_button_get_value(GTK_SPIN_BUTTON(font_button));
-	tmp_layer->start_frame = (guint) gtk_spin_button_get_value(GTK_SPIN_BUTTON(start_button));
-	tmp_layer->finish_frame = (guint) gtk_spin_button_get_value(GTK_SPIN_BUTTON(finish_button));
-	g_string_printf(tmp_layer->name, "%s", gtk_entry_get_text(GTK_ENTRY(name_entry)));
-	g_string_printf(tmp_layer->external_link, "%s", gtk_entry_get_text(GTK_ENTRY(external_link_entry)));
-	g_string_printf(tmp_layer->external_link_window, "%s", gtk_entry_get_text(GTK_ENTRY(external_link_win_entry)));
+		// Retrieve the new starting frame y offset
+		guint_val = gtk_spin_button_get_value(GTK_SPIN_BUTTON(y_off_button_start));
+		validated_guint = validate_value(Y_OFFSET, V_INT_UNSIGNED, &guint_val);
+		if (NULL == validated_guint)
+		{
+			display_warning("Error ED173: There was something wrong with the starting frame Y offset value.  Please try again.");
+			useable_input = FALSE;
+		} else
+		{
+			valid_y_offset_start = *validated_guint;
+			g_free(validated_guint);
+		}
+
+		// Retrieve the new finish frame x offset
+		guint_val = gtk_spin_button_get_value(GTK_SPIN_BUTTON(x_off_button_finish));
+		validated_guint = validate_value(X_OFFSET, V_INT_UNSIGNED, &guint_val);
+		if (NULL == validated_guint)
+		{
+			display_warning("Error ED174: There was something wrong with the finish frame X offset value.  Please try again.");
+			useable_input = FALSE;
+		} else
+		{
+			valid_x_offset_finish = *validated_guint;
+			g_free(validated_guint);
+		}
+
+		// Retrieve the new finish frame y offset
+		guint_val = gtk_spin_button_get_value(GTK_SPIN_BUTTON(y_off_button_finish));
+		validated_guint = validate_value(Y_OFFSET, V_INT_UNSIGNED, &guint_val);
+		if (NULL == validated_guint)
+		{
+			display_warning("Error ED175: There was something wrong with the finish frame X offset value.  Please try again.");
+			useable_input = FALSE;
+		} else
+		{
+			valid_y_offset_finish = *validated_guint;
+			g_free(validated_guint);
+		}
+
+		// Retrieve the new font size
+		guint_val = gtk_spin_button_get_value(GTK_SPIN_BUTTON(font_button));
+		validated_guint = validate_value(FONT_SIZE, V_INT_UNSIGNED, &guint_val);
+		if (NULL == validated_guint)
+		{
+			display_warning("Error ED176: There was something wrong with the font size value.  Please try again.");
+			useable_input = FALSE;
+		} else
+		{
+			valid_font_size = *validated_guint;
+			g_free(validated_guint);
+		}
+
+		// Validate the layer name input
+		validated_string = validate_value(LAYER_NAME, V_CHAR, (gchar *) gtk_entry_get_text(GTK_ENTRY(name_entry)));
+		if (NULL == validated_string)
+		{
+			display_warning("Error ED177: There was something wrong with the layer name value.  Please try again.");
+			useable_input = FALSE;
+		} else
+		{
+			valid_layer_name = g_string_assign(valid_layer_name, validated_string->str);
+			g_string_free(validated_string, TRUE);
+			validated_string = NULL;
+		}
+
+		// Retrieve the new start frame
+		guint_val = gtk_spin_button_get_value(GTK_SPIN_BUTTON(start_button));
+		validated_guint = validate_value(FRAME_NUMBER, V_INT_UNSIGNED, &guint_val);
+		if (NULL == validated_guint)
+		{
+			display_warning("Error ED178: There was something wrong with the start frame value.  Please try again.");
+			useable_input = FALSE;
+		} else
+		{
+			valid_start_frame = *validated_guint;
+			g_free(validated_guint);
+		}
+
+		// Retrieve the new finish frame
+		guint_val = gtk_spin_button_get_value(GTK_SPIN_BUTTON(finish_button));
+		validated_guint = validate_value(FRAME_NUMBER, V_INT_UNSIGNED, &guint_val);
+		if (NULL == validated_guint)
+		{
+			display_warning("Error ED179: There was something wrong with the finish frame value.  Please try again.");
+			useable_input = FALSE;
+		} else
+		{
+			valid_finish_frame = *validated_guint;
+			g_free(validated_guint);
+		}
+
+		// Validate the external link input
+		validated_string = validate_value(EXTERNAL_LINK, V_CHAR, (gchar *) gtk_entry_get_text(GTK_ENTRY(external_link_entry)));
+		if (NULL == validated_string)
+		{
+			display_warning("Error ED180: There was something wrong with the external link value.  Please try again.");
+			useable_input = FALSE;
+		} else
+		{
+			valid_ext_link = g_string_assign(valid_ext_link, validated_string->str);
+			g_string_free(validated_string, TRUE);
+			validated_string = NULL;
+		}
+
+		// Validate the external link window input
+		validated_string = validate_value(EXTERNAL_LINK, V_CHAR, (gchar *) gtk_entry_get_text(GTK_ENTRY(external_link_win_entry)));
+		if (NULL == validated_string)
+		{
+			display_warning("Error ED181: There was something wrong with the external link target window value.  Please try again.");
+			useable_input = FALSE;
+		} else
+		{
+			valid_ext_link_win = g_string_assign(valid_ext_link_win, validated_string->str);
+			g_string_free(validated_string, TRUE);
+			validated_string = NULL;
+		}
+	} while (FALSE == useable_input);
+
+	// * We only get here after all input is considered valid *
+
+	// Fill out the temporary layer with the requested details
+	tmp_layer->x_offset_start = valid_x_offset_start;
+	tmp_layer->y_offset_start = valid_y_offset_start;
+	tmp_layer->x_offset_finish = valid_x_offset_finish;
+	tmp_layer->y_offset_finish = valid_y_offset_finish;
+	tmp_text_ob->font_size = valid_font_size;
+	tmp_layer->start_frame = valid_start_frame;
+	tmp_layer->finish_frame = valid_finish_frame;
+	g_string_printf(tmp_layer->name, "%s", valid_layer_name->str);
+	g_string_printf(tmp_layer->external_link, "%s", valid_ext_link->str);
+	g_string_printf(tmp_layer->external_link_window, "%s", valid_ext_link_win->str);
 	gtk_color_button_get_color(GTK_COLOR_BUTTON(color_button), &(tmp_text_ob->text_color));
+
+	// Copy the text buffer from the onscreen widget to our existing text buffer
+	gtk_text_buffer_get_start_iter(text_buffer, &text_start);
+	gtk_text_buffer_get_end_iter(text_buffer, &text_end);
+	text_gstring = g_string_new(gtk_text_buffer_get_slice(text_buffer, &text_start, &text_end, TRUE));
+	gtk_text_buffer_set_text(GTK_TEXT_BUFFER(tmp_text_ob->text_buffer), text_gstring->str, text_gstring->len);
 
 	// Destroy the dialog box
 	gtk_widget_destroy(GTK_WIDGET(text_dialog));
 
-	// Let the caller know that things went ok
+	// Free the memory allocated in this function
+	g_object_unref(text_buffer);
+	g_string_free(valid_ext_link, TRUE);
+	g_string_free(valid_ext_link_win, TRUE);
+	g_string_free(valid_layer_name, TRUE);
+	g_string_free(text_gstring, TRUE);
+
 	return TRUE;
 }
 
@@ -268,6 +448,9 @@ gboolean display_dialog_text(layer *tmp_layer, gchar *dialog_title)
  * +++++++
  * 
  * $Log$
+ * Revision 1.6  2008/02/20 21:57:15  vapour
+ * Updated to validate all incoming input.
+ *
  * Revision 1.5  2008/02/12 05:19:26  vapour
  * Adjusted to work with the new, slightly simplified layer structure.
  *
