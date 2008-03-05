d69ed0f4e971a1635a6040bc48b71f130d8024f7
Justin Clift <justin@salasaga.org>
2008-03-05 12:52:50 +0000

    Added initial working code to draw a bounding box when dragging out a new highlight layer.

--- salasaga_ide/functions/working_area_motion_notify_event.c
+++ salasaga_ide/functions/working_area_motion_notify_event.c
@@ -22,6 +22,12 @@
  */
 
 
+// Turn on C99 compatibility - needed for roundf() to work
+#define _ISOC99_SOURCE
+
+// Math include
+#include <math.h>
+
 // GTK includes
 #include <gtk/gtk.h>
 
@@ -38,10 +44,69 @@
 gboolean working_area_motion_notify_event(GtkWidget *widget, GdkEventButton *event, gpointer data)
 {
 	// Local variables
-	GdkModifierType		button_state;
-	gint					mouse_x;
-	gint					mouse_y;
+	GdkModifierType		button_state;				// Mouse button states
+	const GdkColor		line_fg_color = { 0, 0xff, 0x00, 0x00 };
+	static GdkGC		*line_gc = NULL;
+	GdkSegment			lines[4];
+	gint				mouse_x;					// Unscaled mouse x position
+	gint				mouse_y;					// Unscaled mouse x position
+	GdkRectangle		tmp_rectangle;
 
+
+	// Find out where the mouse is positioned, and which buttons and modifier keys are down (active)
+	gdk_window_get_pointer(event->window, &mouse_x, &mouse_y, &button_state);
+
+	// If we're creating a new highlight layer, draw a bounding box
+	if (TYPE_HIGHLIGHT == new_layer_selected)
+	{
+		// Ensure the invalidation (redraw) area is set to the maximum size that has been selected for this highlight
+		if (mouse_x > invalidation_area_x)
+		{
+			invalidation_area_x = mouse_x;
+		}
+		if (mouse_y > invalidation_area_y)
+		{
+			invalidation_area_y = mouse_y;
+		}
+
+		// Restore the front store area we're going over from the backing store
+		gdk_draw_pixbuf(GDK_DRAWABLE(front_store), NULL, GDK_PIXBUF(backing_store), stored_x - 1, stored_y - 1, stored_x, stored_y,
+				(invalidation_area_x - stored_x) + 1, (invalidation_area_y - stored_y) + 1, GDK_RGB_DITHER_NONE, 0, 0);
+
+		// Draw a bounding box on the front store
+		if (NULL == line_gc)
+		{
+			line_gc = gdk_gc_new(GDK_DRAWABLE(front_store));
+		}
+		gdk_gc_set_rgb_fg_color(line_gc, &line_fg_color);
+		gdk_gc_set_rgb_bg_color(line_gc, &line_fg_color);
+		lines[0].x1 = stored_x;
+		lines[0].y1 = stored_y;
+		lines[0].x2 = mouse_x;
+		lines[0].y2 = stored_y;
+		lines[1].x1 = mouse_x;
+		lines[1].y1 = stored_y;
+		lines[1].x2 = mouse_x;
+		lines[1].y2 = mouse_y;
+		lines[2].x1 = mouse_x;
+		lines[2].y1 = mouse_y;
+		lines[2].x2 = stored_x;
+		lines[2].y2 = mouse_y;
+		lines[3].x1 = stored_x;
+		lines[3].y1 = mouse_y;
+		lines[3].x2 = stored_x;
+		lines[3].y2 = stored_y;
+		gdk_draw_segments(GDK_DRAWABLE(front_store), line_gc, lines, 4);
+
+		// Tell the window system to display the updated front store area
+		tmp_rectangle.x = stored_x;
+		tmp_rectangle.y = stored_y;
+		tmp_rectangle.width = (invalidation_area_x - stored_x) + 1;
+		tmp_rectangle.height = (invalidation_area_y - stored_y) + 1;
+		gdk_window_invalidate_rect(main_drawing_area->window, &tmp_rectangle, TRUE);
+
+		return TRUE;
+	}
 
 	// If we're already aware of a mouse drag operation going on, then return
 	if (TRUE == mouse_dragging)
@@ -50,9 +115,6 @@ gboolean working_area_motion_notify_event(GtkWidget *widget, GdkEventButton *eve
 	}
 
 	// * To get here, this must be the first time we've heard of this particular mouse drag *
-
-	// Find out where the mouse is positioned, and which buttons and modifier keys are down (active)
-	gdk_window_get_pointer(event->window, &mouse_x, &mouse_y, &button_state);
 
 	// Check if the primary mouse button is down
 	if (GDK_BUTTON1_MASK & button_state)
@@ -70,6 +132,9 @@ gboolean working_area_motion_notify_event(GtkWidget *widget, GdkEventButton *eve
  * +++++++
  * 
  * $Log$
+ * Revision 1.5  2008/03/05 12:52:50  vapour
+ * Added initial working code to draw a bounding box when dragging out a new highlight layer.
+ *
  * Revision 1.4  2008/02/04 17:23:09  vapour
  *  + Removed unnecessary includes.
  *
