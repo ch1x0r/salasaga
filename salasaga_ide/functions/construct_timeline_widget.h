d2b1119c6d419f9b23da298e6da5f76513c4149e
Justin Clift <justin@salasaga.org>
2008-03-18 13:27:20 +0000

    Removed History as adding log entries is not supported by subversion.

--- salasaga_ide/functions/construct_timeline_widget.h
+++ salasaga_ide/functions/construct_timeline_widget.h
@@ -34,17 +34,3 @@ GtkWidget *construct_timeline_widget(slide *slide_data);
 #endif // __cplusplus
 
 #endif /*CONSTRUCT_TIMELINE_WIDGET_H_*/
-
-
-/* 
- * History
- * +++++++
- * 
- * $Log$
- * Revision 1.2  2008/01/15 16:19:00  vapour
- * Updated copyright notice to include 2008.
- *
- * Revision 1.1  2007/09/29 04:22:15  vapour
- * Broke gui-functions.c and gui-functions.h into its component functions.
- *
- */
