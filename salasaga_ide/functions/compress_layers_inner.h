2cb5cba7083f798c2946549ec5f101f896400cc2
Justin Clift <justin@salasaga.org>
2008-03-18 13:25:42 +0000

    Removed History as adding log entries is not supported by subversion.

--- salasaga_ide/functions/compress_layers_inner.h
+++ salasaga_ide/functions/compress_layers_inner.h
@@ -34,17 +34,3 @@ void compress_layers_inner(gpointer element, gpointer user_data);
 #endif // __cplusplus
 
 #endif /*COMPRESS_LAYERS_INNER_H_*/
-
-
-/* 
- * History
- * +++++++
- * 
- * $Log$
- * Revision 1.2  2008/01/15 16:19:03  vapour
- * Updated copyright notice to include 2008.
- *
- * Revision 1.1  2007/09/29 04:22:16  vapour
- * Broke gui-functions.c and gui-functions.h into its component functions.
- *
- */
