b20d0b5bc330a0f0d09b966ada3c1924062622e5
Justin Clift <justin@salasaga.org>
2008-03-21 00:36:00 +0000

    Updated email address to justin@salasaga.org.

--- salasaga_screencapture/salasaga_screencapture.c
+++ salasaga_screencapture/salasaga_screencapture.c
@@ -3,7 +3,7 @@
  *
  * Salasaga: Background screen capture process
  * 
- * Copyright (C) 2005-2008 Justin Clift <justin@postgresql.org>
+ * Copyright (C) 2005-2008 Justin Clift <justin@salasaga.org>
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
