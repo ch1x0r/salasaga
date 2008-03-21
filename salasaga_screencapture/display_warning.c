64f60216e52377ac540b8a020ea856e48e983dff
Justin Clift <justin@salasaga.org>
2008-03-21 00:34:04 +0000

    Updated email address to justin@salasaga.org.

--- salasaga_screencapture/display_warning.c
+++ salasaga_screencapture/display_warning.c
@@ -3,7 +3,7 @@
  *
  * Salasaga: Display a warning message to the user 
  * 
- * Copyright (C) 2008 Justin Clift <justin@postgresql.org>
+ * Copyright (C) 2008 Justin Clift <justin@salasaga.org>
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
