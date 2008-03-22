b39fb7d73b40e3f4e72406f7540da2f1b1019b3e
Justin Clift <justin@salasaga.org>
2008-03-22 13:45:13 +0000

    Significantly improved the accuracy of object boundary detection.

--- salasaga_ide/functions/calculate_object_boundaries.c
+++ salasaga_ide/functions/calculate_object_boundaries.c
@@ -22,6 +22,12 @@
  */
 
 
+// Turn on C99 compatibility - needed for roundf() to work
+#define _ISOC99_SOURCE
+
+// Math include
+#include <math.h>
+
 // GTK includes
 #include <gtk/gtk.h>
 
@@ -39,17 +45,18 @@
 void calculate_object_boundaries(void)
 {
 	// Local variables
-	boundary_box		*boundary;				// Boundary information
-	guint				count_int;				// Counter
+	boundary_box		*boundary;					// Boundary information
+	guint				count_int;					// Counter used to loop through the layers
 	GList				*layer_pointer;
-	guint				num_layers;				// The number of layers in the slide
-	layer_highlight		*this_highlight;		// Pointer to the highlight layer data we're working on
-	layer_image			*this_image;			// Pointer to the image layer data we're working on
-	layer_mouse			*this_mouse;			// Pointer to the mouse layer data we're working on
-	layer_text			*this_text;				// Pointer to the text layer data we're working on
-	layer				*this_layer;			// Pointer to the layer we're working on
-
-	GdkRectangle		tmp_rectangle;			//
+	guint				num_layers;					// The number of layers in the slide
+	gfloat				scaled_height_ratio;		// Used to calculate a vertical scaling ratio 
+	gfloat				scaled_width_ratio;			// Used to calculate a horizontal scaling ratio
+	layer_highlight		*this_highlight;			// Pointer to the highlight layer data we're working on
+	layer_image			*this_image;				// Pointer to the image layer data we're working on
+	layer_mouse			*this_mouse;				// Pointer to the mouse layer data we're working on
+	layer_text			*this_text;					// Pointer to the text layer data we're working on
+	layer				*this_layer;				// Pointer to the layer we're working on
+	GdkRectangle		tmp_rectangle;				//
 
 
 	// Only continue in this function if we have a slide structure available
@@ -57,6 +64,10 @@ void calculate_object_boundaries(void)
 	{
 		return;
 	}
+
+	// Calculate the height and width scaling values for the main drawing area at its present size
+	scaled_height_ratio = (gfloat) project_height / (gfloat) main_drawing_area->allocation.height;
+	scaled_width_ratio = (gfloat) project_width / (gfloat) main_drawing_area->allocation.width;
 
 	// Work out how many layers we need to iterate through
 	layer_pointer = g_list_first(((slide *) current_slide->data)->layers);
@@ -71,7 +82,7 @@ void calculate_object_boundaries(void)
 		boundary_list = NULL;
 	}
 
-	for (count_int = 0; count_int <= num_layers - 1; count_int++)
+	for (count_int = 0; count_int < num_layers; count_int++)
 	{
 		// Select the desired layer
 		layer_pointer = g_list_first(layer_pointer);
@@ -100,10 +111,10 @@ void calculate_object_boundaries(void)
 				this_image = (layer_image *) this_layer->object_data;
 
 				// Translate the area covered by the layer object, with the zoom factor
-				tmp_rectangle.x = (this_layer->x_offset_start * zoom) / 100;
-				tmp_rectangle.width = (this_image->width * zoom) / 100;
-				tmp_rectangle.y = (this_layer->y_offset_start * zoom) / 98;
-				tmp_rectangle.height = (this_image->height * zoom) / 97;
+				tmp_rectangle.x = roundf(this_layer->x_offset_start / scaled_width_ratio);
+				tmp_rectangle.width = roundf(this_image->width / scaled_width_ratio);
+				tmp_rectangle.y = roundf(this_layer->y_offset_start / scaled_height_ratio);
+				tmp_rectangle.height = roundf(this_image->height / scaled_height_ratio);
 				break;
 
 			case TYPE_HIGHLIGHT:
@@ -111,11 +122,11 @@ void calculate_object_boundaries(void)
 				// Simplify the pointers
 				this_highlight = (layer_highlight *) this_layer->object_data;
 
-				// Translate the area covered by the layer object, with the zoom factor
-				tmp_rectangle.x = (this_layer->x_offset_start * zoom) / 100;
-				tmp_rectangle.width = (this_highlight->width * zoom) / 100;
-				tmp_rectangle.y = (this_layer->y_offset_start * zoom) / 98;
-				tmp_rectangle.height = (this_highlight->height * zoom) / 97;
+				// Work out the working area covered by this object
+				tmp_rectangle.x = roundf(this_layer->x_offset_start / scaled_width_ratio);
+				tmp_rectangle.width = roundf(this_highlight->width / scaled_width_ratio);
+				tmp_rectangle.y = roundf(this_layer->y_offset_start / scaled_height_ratio);
+				tmp_rectangle.height = roundf(this_highlight->height / scaled_height_ratio);
 				break;
 
 			case TYPE_MOUSE_CURSOR:
@@ -124,10 +135,10 @@ void calculate_object_boundaries(void)
 				this_mouse = (layer_mouse *) this_layer->object_data;
 
 				// Translate the area covered by the layer object, with the zoom factor
-				tmp_rectangle.x = (this_layer->x_offset_start * zoom) / 100;
-				tmp_rectangle.width = (this_mouse->width * zoom) / 100;
-				tmp_rectangle.y = (this_layer->y_offset_start * zoom) / 98;
-				tmp_rectangle.height = (this_mouse->height * zoom) / 97;
+				tmp_rectangle.x = roundf(this_layer->x_offset_start / scaled_width_ratio);
+				tmp_rectangle.width = roundf(this_mouse->width / scaled_width_ratio);
+				tmp_rectangle.y = roundf(this_layer->y_offset_start / scaled_height_ratio);
+				tmp_rectangle.height = roundf(this_mouse->height / scaled_height_ratio);
 				break;
 
 			case TYPE_TEXT:
@@ -136,10 +147,10 @@ void calculate_object_boundaries(void)
 				this_text = (layer_text *) this_layer->object_data;
 
 				// Translate the area covered by the layer object, with the zoom factor
-				tmp_rectangle.x = (this_layer->x_offset_start * zoom) / 102;
-				tmp_rectangle.width = (this_text->rendered_width * zoom) / 101;
-				tmp_rectangle.y = (this_layer->y_offset_start * zoom) / 98;
-				tmp_rectangle.height = (this_text->rendered_height * zoom) / 97;
+				tmp_rectangle.x = roundf(this_layer->x_offset_start / scaled_width_ratio);
+				tmp_rectangle.width = roundf(this_text->rendered_width / scaled_width_ratio);
+				tmp_rectangle.y = roundf(this_layer->y_offset_start / scaled_height_ratio);
+				tmp_rectangle.height = roundf(this_text->rendered_height / scaled_height_ratio);
 				break;
 
 			default:
