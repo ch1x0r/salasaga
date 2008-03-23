0df598a560e0128aeb524bb8b295f5d4646dd24a
Justin Clift <justin@salasaga.org>
2008-03-23 17:25:56 +0000

    Added initial placeholder for the layer pasting function.

---
+++ salasaga_ide/functions/layer_paste.c
@@ -0,0 +1,39 @@
+/*
+ * $Id$
+ *
+ * Salasaga: Copies the contents of the copy buffer into the selected slide
+ * 
+ * Copyright (C) 2008 Justin Clift <justin@salasaga.org>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ */
+
+
+// GTK includes
+#include <gtk/gtk.h>
+
+// Salasaga includes
+#include "../salasaga_types.h"
+#include "../externs.h"
+
+void layer_paste(void)
+{
+	// Local variables
+
+	// fixme2: Just a place holder for now
+
+	return;
+}
