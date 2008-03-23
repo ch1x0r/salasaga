05b5c3a4a9783c364ce8c5fb8ab44025467f8fab
Justin Clift <justin@salasaga.org>
2008-03-23 16:34:33 +0000

    Fixed a potential small memory leak by freeing the old path after creating a new one.

--- salasaga_ide/functions/slide_move_down.c
+++ salasaga_ide/functions/slide_move_down.c
@@ -40,9 +40,10 @@ void slide_move_down(void)
 {
 	// Local variables
 	GtkTreeIter			from_iter, to_iter;
-	GtkTreePath			*new_path;				// Temporary path
+	GtkTreePath			*new_path;					// Temporary path
 	GList				*next_slide;				// Pointer to the slide below
-	gint				num_slides;				// The total number of slides
+	gint				num_slides;					// The total number of slides
+	GtkTreePath			*old_path = NULL;			// The old path, which we'll free
 	gint				slide_position;				// Which slide in the slide list we are moving
 	slide				*this_slide_data;			// Pointer to the data for this slide
 	GString				*tmp_gstring;
@@ -89,10 +90,14 @@ void slide_move_down(void)
 	create_tooltips();
 
 	// Scroll the film strip to show the new thumbnail position
+	gtk_tree_view_get_cursor(GTK_TREE_VIEW(film_strip_view), &new_path, NULL);
+	if (NULL != new_path)
+		old_path = new_path;  // Make a backup of the old path, so we can free it
 	new_path = gtk_tree_path_new_from_indices(slide_position + 1, -1);
 	gtk_tree_view_set_cursor(GTK_TREE_VIEW(film_strip_view), new_path, NULL, FALSE);
 	gtk_tree_view_scroll_to_cell(GTK_TREE_VIEW(film_strip_view), new_path, NULL, TRUE, 0.5, 0.0);
-//	gtk_tree_path_free(new_path);
+	if (NULL != old_path)
+		gtk_tree_path_free(old_path);  // Free the old path
 
 	// Update the status bar
 	gtk_statusbar_push(GTK_STATUSBAR(status_bar), statusbar_context, " Slide moved down");
