ee777baa24fd0e09e974bdd785c2340d8947c89f
Justin Clift <justin@salasaga.org>
2008-03-24 12:56:59 +0000

    Added initial working code to draw bounding box when resizing a highlight layer.

--- salasaga_ide/functions/working_area_motion_notify_event.c
+++ salasaga_ide/functions/working_area_motion_notify_event.c
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- * Salasaga: Detect when the user moves a pressed down mouse button on the drawing area 
+ * Salasaga: Called when the user is dragging the mouse on the drawing area 
  * 
  * Copyright (C) 2007-2008 Justin Clift <justin@salasaga.org>
  * 
@@ -48,6 +48,7 @@ gboolean working_area_motion_notify_event(GtkWidget *widget, GdkEventButton *eve
 	gint				height;
 	layer				*layer_data;
 	GtkWidget			*list_widget;				// Alias to the timeline widget to make things easier
+	GdkRectangle		mouse_pointer_rect;			// Rectangle holding the mouse pointer position
 	gint				mouse_x;					// Unscaled mouse x position
 	gint				mouse_y;					// Unscaled mouse x position
 	gint				onscreen_bottom;			// Y coordinate of bounding box bottom
@@ -56,6 +57,7 @@ gboolean working_area_motion_notify_event(GtkWidget *widget, GdkEventButton *eve
 	gint				onscreen_top;				// Y coordinate of bounding box top
 	gint				present_x;
 	gint				present_y;
+	static guint		resize_direction = 0;		// Which direction the resizing is going in
 	gfloat				scaled_height_ratio;		// Used to calculate a vertical scaling ratio 
 	gfloat				scaled_width_ratio;			// Used to calculate a horizontal scaling ratio
 	gchar				*selected_row;				// Holds the number of the row that is selected
@@ -73,6 +75,125 @@ gboolean working_area_motion_notify_event(GtkWidget *widget, GdkEventButton *eve
 	{
 		// Draw the updated bounding box
 		draw_bounding_box(stored_x, stored_y, mouse_x, mouse_y);
+		return TRUE;
+	}
+
+	// If we're already aware of a resize operation going on, then draw the appropriate bounding box
+	if (RESIZE_HANDLES_RESIZING == resize_handles_status)
+	{
+		// Initialise some things
+		current_slide_data = current_slide->data;
+		list_widget = current_slide_data->timeline_widget;
+
+		// Calculate the height and width scaling values for the main drawing area at its present size
+		scaled_height_ratio = (gfloat) project_height / (gfloat) main_drawing_area->allocation.height;
+		scaled_width_ratio = (gfloat) project_width / (gfloat) main_drawing_area->allocation.width;
+
+		// Determine which layer is selected in the timeline
+		gtk_tree_view_get_cursor(GTK_TREE_VIEW(list_widget), &tmp_path, NULL);
+		selected_row = gtk_tree_path_to_string(tmp_path);
+
+		// Get its present X and Y offsets
+		current_slide_data->layers = g_list_first(current_slide_data->layers);
+		layer_data = g_list_nth_data(current_slide_data->layers, atoi(selected_row));
+		switch (layer_data->object_type)
+		{
+			case TYPE_HIGHLIGHT:
+				present_x = layer_data->x_offset_start;
+				present_y = layer_data->y_offset_start;
+				width = ((layer_highlight *) layer_data->object_data)->width;
+				height = ((layer_highlight *) layer_data->object_data)->height;
+				break;
+
+			default:
+				return FALSE;  // Unknown layer type, so ignore this event
+		}
+
+		// Calculate the distance the object has been dragged onscreen
+		x_diff = mouse_x - stored_x;
+		y_diff = mouse_y - stored_y;
+
+		// Work out the bounding box boundaries
+		switch (resize_direction)
+		{
+			case 1:
+				// Top left resize
+				onscreen_left = (present_x / scaled_width_ratio) + x_diff;
+				onscreen_top = (present_y / scaled_height_ratio) + y_diff;
+				onscreen_right = ((present_x + width) / scaled_width_ratio);
+				onscreen_bottom = ((present_y + height) / scaled_height_ratio);
+				break;
+
+			case 2:
+				// Top middle resize
+				onscreen_left = (present_x / scaled_width_ratio);
+				onscreen_top = (present_y / scaled_height_ratio) + y_diff;
+				onscreen_right = ((present_x + width) / scaled_width_ratio);
+				onscreen_bottom = ((present_y + height) / scaled_height_ratio);
+				break;
+
+			case 3:
+				// Top right resize
+				onscreen_left = (present_x / scaled_width_ratio);
+				onscreen_top = (present_y / scaled_height_ratio) + y_diff;
+				onscreen_right = ((present_x + width) / scaled_width_ratio) + x_diff;
+				onscreen_bottom = ((present_y + height) / scaled_height_ratio);
+				break;
+
+			case 4:
+				// Middle right resize
+				onscreen_left = (present_x / scaled_width_ratio);
+				onscreen_top = (present_y / scaled_height_ratio);
+				onscreen_right = ((present_x + width) / scaled_width_ratio) + x_diff;
+				onscreen_bottom = ((present_y + height) / scaled_height_ratio);
+				break;
+
+			case 5:
+				// Bottom right resize
+				onscreen_left = (present_x / scaled_width_ratio);
+				onscreen_top = (present_y / scaled_height_ratio);
+				onscreen_right = ((present_x + width) / scaled_width_ratio) + x_diff;
+				onscreen_bottom = ((present_y + height) / scaled_height_ratio) + y_diff;
+				break;
+
+			case 6:
+				// Bottom middle resize
+				onscreen_left = (present_x / scaled_width_ratio);
+				onscreen_top = (present_y / scaled_height_ratio);
+				onscreen_right = ((present_x + width) / scaled_width_ratio);
+				onscreen_bottom = ((present_y + height) / scaled_height_ratio) + y_diff;
+				break;
+
+			case 7:
+				// Bottom left resize
+				onscreen_left = (present_x / scaled_width_ratio) + x_diff;
+				onscreen_top = (present_y / scaled_height_ratio);
+				onscreen_right = ((present_x + width) / scaled_width_ratio);
+				onscreen_bottom = ((present_y + height) / scaled_height_ratio) + y_diff;
+				break;
+
+			case 8:
+				// Left middle resize
+				onscreen_left = (present_x / scaled_width_ratio) + x_diff;
+				onscreen_top = (present_y / scaled_height_ratio);
+				onscreen_right = ((present_x + width) / scaled_width_ratio);
+				onscreen_bottom = ((present_y + height) / scaled_height_ratio);
+				break;
+
+			default:
+				display_warning("Error ED295: Unknown resizing direction");
+				return FALSE;
+		}
+
+		// Ensure the bounding box doesn't go out of bounds
+		onscreen_left = CLAMP(onscreen_left, 2, main_drawing_area->allocation.width - 2);
+		onscreen_top = CLAMP(onscreen_top, 2, main_drawing_area->allocation.height - 2);
+		onscreen_right = CLAMP(onscreen_right, 2, main_drawing_area->allocation.width - 2);
+		onscreen_bottom = CLAMP(onscreen_bottom, 2, main_drawing_area->allocation.height - 2);
+
+		// Draw a bounding box onscreen
+		draw_bounding_box(onscreen_left, onscreen_top, onscreen_right, onscreen_bottom);
+
 		return TRUE;
 	}
 
@@ -169,12 +290,108 @@ gboolean working_area_motion_notify_event(GtkWidget *widget, GdkEventButton *eve
 		return TRUE;
 	}
 
-	// * To get here, this must be the first time we've heard of this particular mouse drag *
+	// * To get here, this must be the first time we've heard of this particular drag operation *
 
-	// Check if the primary mouse button is down
-	if (GDK_BUTTON1_MASK & button_state)
+	// Check if the primary mouse button is down and we're open to being clicked on the resize handles
+	if ((GDK_BUTTON1_MASK & button_state) && (RESIZE_HANDLES_WAITING == resize_handles_status))
 	{
-		// It is, so we take notice of the mouse drag operation
+		// Create the mouse pointer rectangle
+		mouse_pointer_rect.x = event->x;
+		mouse_pointer_rect.y = event->y;
+		mouse_pointer_rect.width = 10;  // I think that the bigger this is, the more likely for the handle click to happen 
+		mouse_pointer_rect.height = 10;  // I think that the bigger this is, the more likely for the handle click to happen
+
+		// * Check if the mouse is over a corner resize handle *
+
+		// Top left
+		if (TRUE == gdk_rectangle_intersect(&resize_handles_rect[0], &mouse_pointer_rect, NULL))
+		{
+			// We're resizing from the top left
+			resize_handles_status = RESIZE_HANDLES_RESIZING;
+			resize_direction = 1;
+		}
+
+		// Top right
+		if (TRUE == gdk_rectangle_intersect(&resize_handles_rect[2], &mouse_pointer_rect, NULL))
+		{
+			// We're resizing from the top right
+			resize_handles_status = RESIZE_HANDLES_RESIZING;
+			resize_direction = 3;
+		}
+
+		// Bottom right
+		if (TRUE == gdk_rectangle_intersect(&resize_handles_rect[4], &mouse_pointer_rect, NULL))
+		{
+			// We're resizing from the bottom right
+			resize_handles_status = RESIZE_HANDLES_RESIZING;
+			resize_direction = 5;
+		}
+
+		// Bottom left
+		if (TRUE == gdk_rectangle_intersect(&resize_handles_rect[6], &mouse_pointer_rect, NULL))
+		{
+			// We're resizing from the bottom left
+			resize_handles_status = RESIZE_HANDLES_RESIZING;
+			resize_direction = 7;
+		}
+
+		// If the horizontal resize handles are in use, check them
+		if (0 != resize_handles_rect[3].width)
+		{
+			// Right middle
+			if (TRUE == gdk_rectangle_intersect(&resize_handles_rect[3], &mouse_pointer_rect, NULL))
+			{
+				// We're resizing from the right middle
+				resize_handles_status = RESIZE_HANDLES_RESIZING;
+				resize_direction = 4;
+			}
+
+			// Left middle
+			if (TRUE == gdk_rectangle_intersect(&resize_handles_rect[7], &mouse_pointer_rect, NULL))
+			{
+				// We're resizing from the left middle
+				resize_handles_status = RESIZE_HANDLES_RESIZING;
+				resize_direction = 8;
+			}
+		}
+
+		// If the vertical resize handles are in use, check them
+		if (0 != resize_handles_rect[1].width)
+		{
+			// Top middle
+			if (TRUE == gdk_rectangle_intersect(&resize_handles_rect[1], &mouse_pointer_rect, NULL))
+			{
+				// We're resizing from the top middle
+				resize_handles_status = RESIZE_HANDLES_RESIZING;
+				resize_direction = 2;
+			}
+
+			// Bottom middle
+			if (TRUE == gdk_rectangle_intersect(&resize_handles_rect[5], &mouse_pointer_rect, NULL))
+			{
+				// We're resizing from the bottom middle
+				resize_handles_status = RESIZE_HANDLES_RESIZING;
+				resize_direction = 6;
+			}
+		}
+
+		// Store the mouse coordinates so we know where to resize from
+		stored_x = event->x;
+		stored_y = event->y;
+
+		// Reset the invalidation area
+		invalidation_end_x = event->x;
+		invalidation_end_y = event->y;
+		invalidation_start_x = event->x - 1;
+		invalidation_start_y = event->y - 1;
+
+		// We drop through here on purpose, to allow checking for the start of a drag operation
+	}
+
+	// Check if the primary mouse button is down and we're not resizing a layer
+	if ((GDK_BUTTON1_MASK & button_state) && (RESIZE_HANDLES_RESIZING != resize_handles_status))
+	{
+		// We're commencing a drag, so note this
 		mouse_dragging = TRUE;
 
 		// Store the mouse coordinates so we know where to drag from
@@ -186,6 +403,8 @@ gboolean working_area_motion_notify_event(GtkWidget *widget, GdkEventButton *eve
 		invalidation_end_y = event->y;
 		invalidation_start_x = event->x - 1;
 		invalidation_start_y = event->y - 1;
+
+		return TRUE;
 	}
 
 	return TRUE;
