94a92fda3e8cc1921f8d3ffc31f525c754c847cc
Justin Clift <justin@salasaga.org>
2008-04-09 10:25:49 +0000

    Updated to calculate selection row height properly.

--- salasaga_ide/functions/widgets/time_line.c
+++ salasaga_ide/functions/widgets/time_line.c
@@ -273,7 +273,7 @@ void time_line_internal_draw_selection_highlight(TimeLinePrivate *priv, gint wid
 	x1 = 0;
 	y1 = priv->top_border_height + 1 + (selected_row * priv->row_height);
 	x2 = width - 1;
-	y2 = ((selected_row + 1) * priv->row_height) - 1;
+	y2 = priv->row_height - 2;
 	gdk_gc_set_rgb_fg_color(GDK_GC(display_buffer_gc), &colour_red);
 	gdk_gc_set_line_attributes(GDK_GC(display_buffer_gc), 1, GDK_LINE_ON_OFF_DASH, GDK_CAP_BUTT, GDK_JOIN_MITER);
 	gdk_gc_set_dashes(GDK_GC(display_buffer_gc), 1, dash_list, 2);
