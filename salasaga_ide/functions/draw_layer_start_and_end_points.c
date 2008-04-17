7dc0150db29a4db19e8405bd49534c825e7f501e
Justin Clift <justin@salasaga.org>
2008-04-17 17:46:48 +0000

    Updated code to draw the end points on the working area widget.

--- salasaga_ide/functions/draw_layer_start_and_end_points.c
+++ salasaga_ide/functions/draw_layer_start_and_end_points.c
@@ -38,10 +38,16 @@ gboolean draw_layer_start_and_end_points()
 	const GdkColor		colour_green = {0, 0, 40000, 0};
 	const GdkColor		colour_red = {0, 40000, 0, 0 };
 	static GdkColormap	*colourmap = NULL;			// Colormap used for drawing
+	gint				finish_mid_point_x;
+	gint				finish_mid_point_y;
 	gint				finish_x;					// X position at the layer objects finish time
 	gint				finish_y;					// Y position at the layer objects finish time 
 	gint				height;						//
 	GList				*layer_pointer;
+	gint				old_start_x = 0;			// Value used in the previous run
+	gint				old_start_y = 0;			// Value used in the previous run
+	gint				old_width = 0;				// Value used in the previous run
+	gint				old_height = 0;				// Value used in the previous run
 	gint				pixmap_height;				// Receives the height of a given pixmap
 	gint				pixmap_width;				// Receives the width of a given pixmap
 	gfloat				scaled_height_ratio;		// Used to calculate a vertical scaling ratio 
@@ -49,8 +55,6 @@ gboolean draw_layer_start_and_end_points()
 	gint				selected_row;				// Number of the row selected in the time line
 	gint				start_mid_point_x;
 	gint				start_mid_point_y;
-	gint				finish_mid_point_x;
-	gint				finish_mid_point_y;
 	gint				start_x;					// X position at the layer objects start time
 	gint				start_y;					// Y position at the layer objects start time
 	layer 				*this_layer_data;			// Pointer to the data for the selected layer
@@ -72,7 +76,22 @@ gboolean draw_layer_start_and_end_points()
 		return TRUE;
 	}
 
-	// Calculate the height and width scaling values for the requested layer size
+	// Initialise colourmap and graphic context
+	if (NULL == colourmap)
+	{
+		colourmap = gdk_colormap_get_system();
+		gdk_drawable_set_colormap(GDK_DRAWABLE(main_drawing_area->window), GDK_COLORMAP(colourmap));
+	}
+	if (NULL == widget_gc)
+	{
+		widget_gc = gdk_gc_new(GDK_DRAWABLE(main_drawing_area->window));
+	}
+
+	// Refresh the area covered by the old end points
+	gdk_draw_drawable(GDK_DRAWABLE(main_drawing_area->window), GDK_GC(widget_gc),
+		GDK_PIXMAP(front_store), old_start_x, old_start_y, old_start_x, old_start_y, old_width, old_height);
+
+	// Calculate the height and width scaling values for the requested layer
 	gdk_drawable_get_size(GDK_PIXMAP(front_store), &pixmap_width, &pixmap_height);
 	scaled_height_ratio = (gfloat) pixmap_height / (gfloat) project_height;
 	scaled_width_ratio = (gfloat) pixmap_width / (gfloat) project_width;
@@ -91,16 +110,11 @@ gboolean draw_layer_start_and_end_points()
 	finish_mid_point_x = finish_x + (width / 2);
 	finish_mid_point_y = finish_y + (height / 2);
 
-	// Initialise colourmap and graphic context
-	if (NULL == colourmap)
-	{
-		colourmap = gdk_colormap_get_system();
-		gdk_drawable_set_colormap(GDK_DRAWABLE(main_drawing_area->window), GDK_COLORMAP(colourmap));
-	}
-	if (NULL == widget_gc)
-	{
-		widget_gc = gdk_gc_new(GDK_DRAWABLE(main_drawing_area->window));
-	}
+	// Store the end points, for the next refresh
+	old_start_x = start_x;
+	old_start_y = start_y;
+	old_width = finish_x + width;
+	old_height = finish_y + height;
 
 	// Draw a line joining the start and end points
 	gdk_gc_set_rgb_fg_color(GDK_GC(widget_gc), &colour_black);
