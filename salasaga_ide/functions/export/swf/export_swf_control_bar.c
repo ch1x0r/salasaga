2663d49b82d0053186bc5915c76d7a55d861b9f4
Justin Clift <justin@salasaga.org>
2008-07-20 09:48:29 +0000

    Updated font names.

--- salasaga_ide/functions/export/swf/export_swf_control_bar.c
+++ salasaga_ide/functions/export/swf/export_swf_control_bar.c
@@ -1052,7 +1052,7 @@ gboolean export_swf_control_bar(SWFMovie main_movie, guint cb_index, guint depth
 		info_object = newSWFText();
 
 		// Create the font object we'll be using
-		font_pathname = g_build_path(G_DIR_SEPARATOR_S, font_path, "fdb", "Bitstream Vera Sans.fdb", NULL);
+		font_pathname = g_build_path(G_DIR_SEPARATOR_S, font_path, "fdb", "Bitstream_Vera_Sans.fdb", NULL);
 
 		// Load the font file if needed
 		font_file = fopen(font_pathname, "r");
