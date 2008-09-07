cc7e79cfdd7886a2a86f0d4a476d27e4486dfdd7
Justin Clift <justin@salasaga.org>
2008-09-07 04:15:58 +0000

    Fixed small memory leaks.

--- salasaga_ide/functions/working_area/event_size_allocate_received.c
+++ salasaga_ide/functions/working_area/event_size_allocate_received.c
@@ -1,12 +1,12 @@
 /*
  * $Id$
  *
- * Salasaga: Function called when the drawing area is resized 
- * 
+ * Salasaga: Function called when the drawing area is resized
+ *
  * Copyright (C) 2005-2008 Justin Clift <justin@salasaga.org>
  *
  * This file is part of Salasaga.
- * 
+ *
  * Salasaga is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of
@@ -42,6 +42,7 @@ gint event_size_allocate_received(GtkWidget *widget, GdkEvent *event, gpointer d
 {
 	// Local variables
 	static gint			old_width = 0;
+	gchar				*tmp_gchar;
 	gint32				tmp_int;
 	GString				*tmp_string;
 
@@ -50,11 +51,13 @@ gint event_size_allocate_received(GtkWidget *widget, GdkEvent *event, gpointer d
 	if (old_width != right_side->allocation.width)
 	{
 		// * The width has been changed *
-		
+
 		// If the required zoom level is "Fit to width", then recalculate the zoom and redraw the drawing area
 		tmp_string = g_string_new(NULL);
-		g_string_printf(tmp_string, "%s", gtk_combo_box_get_active_text(GTK_COMBO_BOX(zoom_selector)));
-	
+		tmp_gchar = gtk_combo_box_get_active_text(GTK_COMBO_BOX(zoom_selector));
+		g_string_printf(tmp_string, "%s", tmp_gchar);
+		g_free(tmp_gchar);
+
 		// Parse and store the new zoom level
 		tmp_int = g_ascii_strncasecmp(tmp_string->str, "F", 1);
 		if (0 == tmp_int)
