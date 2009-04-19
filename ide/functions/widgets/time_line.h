dec5cb9f89c79add9febc8a2339fa1163b2e91ab
Justin Clift <justin@salasaga.org>
2009-04-19 05:52:23 +0000

    Began breaking the time line library into component functions.

--- ide/functions/widgets/time_line.h
+++ ide/functions/widgets/time_line.h
@@ -4,7 +4,7 @@
  * Copyright (C) 2005-2009 Justin Clift <justin@salasaga.org>
  *
  * This file is part of Salasaga.
- * 
+ *
  * Salasaga is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of
@@ -33,14 +33,14 @@ extern "C" {
 #include <glib.h>
 #include <glib-object.h>
 
-// Macros	
+// Public macros
 #define TIME_LINE_TYPE (time_line_get_type())
 #define TIME_LINE(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), TIME_LINE_TYPE, TimeLine))
 #define TIME_LINE_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), TIME_LINE_TYPE, TimeLineClass))
 #define IS_TIME_LINE(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), TIME_LINE_TYPE))
 #define IS_TIME_LINE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), TIME_LINE_TYPE))
 
-// Required structures
+// Public structures
 typedef struct _TimeLine TimeLine;
 typedef struct _TimeLineClass TimeLineClass;
 struct _TimeLine
@@ -53,20 +53,61 @@ struct _TimeLineClass
 	GtkDrawingAreaClass parent_class;
 };
 
-// Public function definitions
-GType time_line_get_type(void) G_GNUC_CONST;
-GtkWidget* time_line_new(void);
-gboolean time_line_regenerate_widget(GtkWidget *widget);
-gfloat time_line_get_cursor_position(GtkWidget *widget);
-gint time_line_get_selected_layer_num(GtkWidget *widget);
-gboolean time_line_set_selected_layer_num(GtkWidget *widget, gint selected_row);
-gboolean time_line_set_stored_slide_duration(GtkWidget *widget, gfloat new_duration);
-void timeline_widget_motion_notify_event(GtkWidget *widget, GdkEventButton *event, gpointer data);
-void timeline_widget_button_press_event(GtkWidget *widget, GdkEventButton *event, gpointer data);
-void timeline_widget_button_release_event(GtkWidget *widget, GdkEventButton *event, gpointer data);
+// fixme2: Pulled these initial sizes out of the air, they may need to be revisited
+#define WIDGET_MINIMUM_HEIGHT	150
+#define WIDGET_MINIMUM_WIDTH	500
+
+// Definitions for the scaling "adjustment" symbols (the plus and minus symbols)
+#define ADJUSTMENTS_X	85
+#define ADJUSTMENTS_Y	2
+#define ADJUSTMENTS_SIZE	10
+
+// Definitions for the cursor head - the triangular part at the top of the time line cursor
+#define CURSOR_HEAD_TOP		1
+#define CURSOR_HEAD_WIDTH	10
+
+// * Private enums *
+enum
+{
+	RESIZE_NONE,
+	RESIZE_TRANS_IN_START,
+	RESIZE_LAYER_START,
+	RESIZE_LAYER_DURATION,
+	RESIZE_TRANS_OUT_DURATION
+};
+
+// Private macros
+#define TIME_LINE_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE((obj), TIME_LINE_TYPE, TimeLinePrivate))
+
+// Private structures
+typedef struct _TimeLinePrivate TimeLinePrivate;
+struct _TimeLinePrivate
+{
+	GdkPixmap			*cached_bg_image;			// Cache of the timeline background image, for drawing upon
+	gboolean			cached_bg_valid;			// Flag for whether the timeline background cache image is valid
+	gboolean			cursor_drag_active;			// Tracks whether we're dragging the time line cursor or not
+	gfloat				cursor_position;			// Where in the slide the cursor is positioned (in seconds or part thereof)
+	GdkPixmap			*display_buffer;			// The rendered version of the timeline
+	gboolean			drag_active;				// Tracks whether we have an active mouse drag or not
+	gint				guide_line_end;				// The pixel number of the ending guide line
+	gint				guide_line_start;			// The pixel number of the starting guide line
+	gint				guide_line_resize;			// The pixel number of the resizing guide line
+	gint				left_border_width;			// Number of pixels in the left border (layer name) area
+	gint				mouse_x;					// Mouse pointer position
+	gint				mouse_y;					// Mouse pointer position
+	guint				resize_type;				// Tracks whether we have an active layer resize or not
+	gint				row_height;					// Number of pixels in each layer row
+	gint				selected_layer_num;			// The number of the selected layer
+	gfloat				stored_slide_duration;		// The original duration of the slide in seconds
+	gint				stored_x;
+	gint				stored_y;
+	guint				timeout_id;					// ID for the timeout source
+	gint				top_border_height;			// Number of pixels in the top border (cursor) area
+};
+
 
 #ifdef __cplusplus
 }
 #endif // __cplusplus
 
-#endif /*TIME_LINE_H_*/
+#endif /* TIME_LINE_H_ */
