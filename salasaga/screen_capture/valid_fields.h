19a4ccfd0a5f6ce24185019e988f6d895e306d8e
Justin Clift <justin@salasaga.org>
2009-05-25 12:37:42 +0000

    Added the SCREENSHOT_DELAY validation info.

--- salasaga/screen_capture/valid_fields.h
+++ salasaga/screen_capture/valid_fields.h
@@ -4,7 +4,7 @@
  * Copyright (C) 2005-2009 Justin Clift <justin@salasaga.org>
  *
  * This file is part of Salasaga.
- * 
+ *
  * Salasaga is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of
@@ -34,6 +34,7 @@ validatation_entry		valid_fields[] =
 {
 	{ FOLDER_PATH,			N_("Full path to a folder"),			V_CHAR,				V_HYPENS | V_PATH_SEP | V_SPACES | V_UNDERSCORES,		0,		-1 },
 	{ PROJECT_NAME,			N_("Name of this project"),				V_CHAR,				V_HYPENS | V_SPACES | V_UNDERSCORES,					0,		20 },
+	{ SCREENSHOT_DELAY,		N_("Number of seconds to delay a screenshot"),	V_INT_UNSIGNED,		V_NONE,		0,		60 },
 	{ SCREENSHOT_HEIGHT,	N_("Height for next screenshot"),		V_INT_UNSIGNED,		V_NONE,		0,		4096 },
 	{ SCREENSHOT_WIDTH,		N_("Width for next screenshot"),		V_INT_UNSIGNED,		V_NONE,		0,		4096 },
 	{ SCREENSHOT_X_OFFSET,	N_("X offset for next screenshot"),		V_INT_UNSIGNED,		V_NONE,		0,		4096 },
