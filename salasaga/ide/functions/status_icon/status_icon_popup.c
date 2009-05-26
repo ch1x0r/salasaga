2508592dd5b7e98e6987f961b7b30baf502e9e56
Justin Clift <justin@salasaga.org>
2009-05-26 22:11:32 +0000

    Added working code for the status bar icon menu options to trigger a screenshot, and minimise or restore the main window.

--- salasaga/ide/functions/status_icon/status_icon_popup.c
+++ salasaga/ide/functions/status_icon/status_icon_popup.c
@@ -35,15 +35,18 @@
 // Salasaga includes
 #include "../../salasaga_types.h"
 #include "../../externs.h"
+#include "../callbacks/track_window_state.h"
 #include "../quit_event.h"
+#include "status_icon_iconify.h"
+#include "status_icon_restore.h"
+#include "status_icon_take_screenshot.h"
 
 
 gint status_icon_popup(GtkWidget *widget, GdkEvent *event, gpointer data)
 {
 	// Local variables
 	GtkWidget			*status_icon_menu;					// Widget holding the status bar icon's menu
-	GtkWidget			*status_icon_menu_item_minimise;	// An item in the status bar icon menu
-	GtkWidget			*status_icon_menu_item_restore;		// An item in the status bar icon menu
+	GtkWidget			*status_icon_menu_item_iconify;		// An item in the status bar icon menu
 	GtkWidget			*status_icon_menu_item_screenshot;	// An item in the status bar icon menu
 	GtkWidget			*status_icon_menu_item_quit;		// An item in the status bar icon menu
 
@@ -52,23 +55,36 @@ gint status_icon_popup(GtkWidget *widget, GdkEvent *event, gpointer data)
 	status_icon_menu = gtk_menu_new();
 	gtk_menu_set_title(GTK_MENU(status_icon_menu), "Salasaga");
 
-	// Create the menu items
-	status_icon_menu_item_minimise = gtk_menu_item_new_with_label("Minimise to status bar");
-	status_icon_menu_item_restore = gtk_menu_item_new_with_label("Restore");
-	status_icon_menu_item_screenshot = gtk_menu_item_new_with_label("Take Screenshot");
-	status_icon_menu_item_quit = gtk_menu_item_new_with_label("Quit");
+	// Add the Restore window/Minimise window menu item(s)
+	if (TRUE == is_window_iconified())
+	{
+		// Create a menu option to restore the window
+		status_icon_menu_item_iconify = gtk_menu_item_new_with_label("Restore window");
+		gtk_menu_shell_append(GTK_MENU_SHELL(status_icon_menu), GTK_WIDGET(status_icon_menu_item_iconify));
+		g_signal_connect(G_OBJECT(status_icon_menu_item_iconify), "activate", G_CALLBACK(status_icon_restore), (gpointer) NULL);
+	} else
+	{
+		// Create a menu option to iconify the window
+		status_icon_menu_item_iconify = gtk_menu_item_new_with_label("Minimise window");
+		gtk_menu_shell_append(GTK_MENU_SHELL(status_icon_menu), GTK_WIDGET(status_icon_menu_item_iconify));
+		g_signal_connect(G_OBJECT(status_icon_menu_item_iconify), "activate", G_CALLBACK(status_icon_iconify), (gpointer) NULL);
+	}
 
-	// Add the menu items to the menu
-	gtk_menu_shell_append(GTK_MENU_SHELL(status_icon_menu), GTK_WIDGET(status_icon_menu_item_minimise));
-	gtk_menu_shell_append(GTK_MENU_SHELL(status_icon_menu), GTK_WIDGET(status_icon_menu_item_restore));
-	gtk_menu_shell_append(GTK_MENU_SHELL(status_icon_menu), GTK_WIDGET(status_icon_menu_item_screenshot));
+	// Add the Take Screenshot menu item if screenshots are enabled
+	if (TRUE == screenshots_enabled)
+	{
+		status_icon_menu_item_screenshot = gtk_menu_item_new_with_label("Take Screenshot");
+		gtk_menu_shell_append(GTK_MENU_SHELL(status_icon_menu), GTK_WIDGET(status_icon_menu_item_screenshot));
+		g_signal_connect(G_OBJECT(status_icon_menu_item_screenshot), "activate", G_CALLBACK(status_icon_take_screenshot), (gpointer) NULL);
+	}
+
+	// Add the Quit menu item
+	status_icon_menu_item_quit = gtk_menu_item_new_with_label("Quit");
 	gtk_menu_shell_append(GTK_MENU_SHELL(status_icon_menu), GTK_WIDGET(status_icon_menu_item_quit));
+	g_signal_connect(G_OBJECT(status_icon_menu_item_quit), "activate", G_CALLBACK(quit_event), (gpointer) NULL);
 
 	// Ensure all of the menu items are displayed
 	gtk_widget_show_all(GTK_WIDGET(status_icon_menu));
-
-	// Connect the quit menu item to the quit signal
-	g_signal_connect(G_OBJECT(status_icon_menu_item_quit), "activate", G_CALLBACK(quit_event), (gpointer) NULL);
 
 	// Make the menu pop up
 	gtk_menu_popup(GTK_MENU(status_icon_menu), NULL, NULL, gtk_status_icon_position_menu, NULL, 0, gtk_get_current_event_time() - 1000);
---
+++ salasaga/ide/functions/status_icon/status_icon_take_screenshot.c
@@ -0,0 +1,48 @@
+/*
+ * $Id$
+ *
+ * Salasaga: Function to trigger a screenshot
+ *
+ * Copyright (C) 2005-2009 Justin Clift <justin@salasaga.org>
+ *
+ * This file is part of Salasaga.
+ *
+ * Salasaga is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+// GTK includes
+#include <gtk/gtk.h>
+
+#ifdef _WIN32
+	// Windows only code
+	#include <windows.h>
+#endif
+
+// Salasaga includes
+#include "../../salasaga_types.h"
+#include "../../externs.h"
+#include "../callbacks/track_window_state.h"
+#include "../quit_event.h"
+
+
+gint status_icon_take_screenshot(GtkWidget *widget, GdkEvent *event, gpointer data)
+{
+	// Launch the screenshot command
+	g_spawn_command_line_async("salasaga_screencapture", NULL);
+
+	return FALSE;
+}
