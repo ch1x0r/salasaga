9f08a4d435f0c12cf95a017cf8c29482a870b844
Justin Clift <justin@salasaga.org>
2009-05-31 14:54:47 +0000

    Changed the crop_all enum definition to a dimensions one.

--- salasaga/ide/salasaga.c
+++ salasaga/ide/salasaga.c
@@ -271,7 +271,7 @@ gint main(gint argc, gchar *argv[])
 	font_status = load_fonts();
 
 	// Initialise the button event handlers on the toolbars to NULL
-	main_toolbar_signals[CROP_ALL] = 0;
+	main_toolbar_signals[DIMENSIONS] = 0;
 	main_toolbar_signals[EXPORT_FLASH] = 0;
 	for (tmp_int = 0; tmp_int < MAIN_TB_COUNT; tmp_int++)
 	{
