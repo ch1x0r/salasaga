139e6539d32b7d6f7985fdccf2f4e56abebb87d2
Justin Clift <justin@salasaga.org>
2009-12-06 01:48:24 +0000

    Updated to use the default font face when there is no font face selected.

--- ide/functions/callbacks/text_layer_dialog_insert_text_signal_triggered.c
+++ ide/functions/callbacks/text_layer_dialog_insert_text_signal_triggered.c
@@ -58,6 +58,7 @@ gboolean text_layer_dialog_insert_text_before(GtkTextBuffer *text_buffer, GtkTex
 	GSList					*applied_tags;		// Receives a list of text tags applied at a position in a text buffer
 	GtkTextIter				cursor_iter;
 	GtkWidget				*fg_colour_button;
+	gboolean				font_face_found;	// Used to track if we've found the font face
 	GtkWidget				*font_face_widget;	// Points to the font face selection widget
 	gint					*font_ptr;			// Used for calculating the font face of a character
 	gint					font_size_int;		// Used for retrieving the size of a character in a text layer
@@ -98,6 +99,7 @@ gboolean text_layer_dialog_insert_text_before(GtkTextBuffer *text_buffer, GtkTex
 	// Determine the font face we should apply to the new text, and save it for the "after" function to use
 	applied_tags = gtk_text_iter_get_tags(&cursor_iter);
 	num_tags = g_slist_length(applied_tags);
+	font_face_found = FALSE;
 	for (loop_counter = 0; loop_counter < num_tags; loop_counter++)
 	{
 		this_tag = g_slist_nth_data(applied_tags, loop_counter);
@@ -106,9 +108,15 @@ gboolean text_layer_dialog_insert_text_before(GtkTextBuffer *text_buffer, GtkTex
 		{
 			// * Only font face tags have the "font-num" property, so this is a font face tag *
 			our_font_face = *font_ptr;
+			font_face_found = TRUE;
 		}
 	}
 	g_slist_free(applied_tags);
+	if (FALSE == font_face_found)
+	{
+		// Couldn't determine the font face to be applied to this text, so we use the default text layer font face
+		our_font_face = default_text_font_face;
+	}
 
 	// Determine the font size we should apply to the new text, and save it for the "after" function to use
 	font_size_int = pango_font_description_get_size(text_attributes->font);
@@ -123,6 +131,7 @@ gboolean text_layer_dialog_insert_text_after(GtkTextBuffer *text_buffer, GtkText
 {
 	// Local variables
 	GtkTextTag				*fg_colour_tag;
+	gint					font_face_to_use;
 	GtkTextIter				selection_start;
 	GtkTextTag				*text_size_text_tag;
 
@@ -136,11 +145,22 @@ gboolean text_layer_dialog_insert_text_after(GtkTextBuffer *text_buffer, GtkText
 	gtk_text_buffer_apply_tag(GTK_TEXT_BUFFER(text_buffer), GTK_TEXT_TAG(fg_colour_tag), &selection_start, cursor_location);
 
 	// * Apply the desired font face to the new text *
-	gtk_text_buffer_apply_tag_by_name(GTK_TEXT_BUFFER(text_buffer), salasaga_font_names[our_font_face], &selection_start, cursor_location);
+	if (-1 == our_font_face)
+	{
+		font_face_to_use = default_text_font_face;
+	} else
+	{
+		font_face_to_use = our_font_face;
+	}
+	gtk_text_buffer_apply_tag_by_name(GTK_TEXT_BUFFER(text_buffer), salasaga_font_names[font_face_to_use], &selection_start, cursor_location);
 
 	// * Apply the desired font size to the new text *
 	text_size_text_tag = text_layer_create_font_size_tag(our_font_size);
 	gtk_text_buffer_apply_tag(GTK_TEXT_BUFFER(text_buffer), GTK_TEXT_TAG(text_size_text_tag), &selection_start, cursor_location);
+
+	// * Update the font face selector to reflect the font face used *
+	// This is needed for (ie.) when there is no text yet in the buffer, so the font face selector is blank and the default font face is used
+	gtk_combo_box_set_active(GTK_COMBO_BOX(text_widgets->font_face_combo_box), font_face_to_use);
 
 	return FALSE;
 }
