2736c7f785965d5c2d386434ca62e34b5a68182a
Justin Clift <justin@salasaga.org>
2009-12-20 07:26:07 +0000

    Added a new function to update the delete focus when the film strip is clicked.

---
+++ salasaga/ide/functions/film_strip/film_strip_button_clicked.c
@@ -0,0 +1,56 @@
+/*
+ * $Id$
+ *
+ * Salasaga: Callback function to update the delete focus when the film strip is clicked
+ *
+ * Copyright (C) 2005-2009 Justin Clift <justin@salasaga.org>
+ *
+ * This file is part of Salasaga.
+ *
+ * Salasaga is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+// Standard includes
+#include <stdlib.h>
+
+// GTK includes
+#include <gtk/gtk.h>
+
+#ifdef _WIN32
+	// Windows only code
+	#include <windows.h>
+#endif
+
+// Salasaga includes
+#include "../../salasaga_types.h"
+#include "../../externs.h"
+#include "../widget_focus.h"
+
+
+gboolean film_strip_button_clicked(GtkWidget *widget, gpointer data)
+{
+	// Only do this function if we have a front store available and a project loaded
+	if ((NULL == front_store) || (FALSE == project_active))
+	{
+		return FALSE;
+	}
+
+	// Set the internal delete focus to be the film strip
+	set_delete_focus(FOCUS_SLIDE);
+
+	return FALSE;
+}
