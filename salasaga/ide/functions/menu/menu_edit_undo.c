d8b9e0923ea7f27beff29c437966879119a36bd8
Justin Clift <justin@salasaga.org>
2010-01-06 13:59:12 +0000

    Added stub Undo and Redo functions for the Edit menu.

---
+++ salasaga/ide/functions/menu/menu_edit_undo.c
@@ -0,0 +1,43 @@
+/*
+ * $Id$
+ *
+ * Salasaga: Function to undo the last action the user performed
+ * 
+ * Copyright (C) 2005-2009 Justin Clift <justin@salasaga.org>
+ *
+ * This file is part of Salasaga.
+ * 
+ * Salasaga is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+// GTK includes
+#include <gtk/gtk.h>
+
+#ifdef _WIN32
+	// Windows only code
+	#include <windows.h>
+#endif
+
+// Salasaga includes
+#include "../../salasaga_types.h"
+#include "../../externs.h"
+
+
+void menu_edit_undo(void)
+{
+	// Stub function
+}
